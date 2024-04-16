2c866733bff45c8cba4efdc3fae26eb98ac9f546
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-04-16 19:50:15 -0400

    fix(systemd-udevd): add systemd-sysctl dependency
    99-systemd.rules depends on systemd-sysctl

--- modules.d/01systemd-udevd/module-setup.sh
+++ modules.d/01systemd-udevd/module-setup.sh
@@ -20,7 +20,7 @@ check() {
 depends() {
 
     # This module has external dependency on other module(s).
-    echo systemd
+    echo systemd systemd-sysctl
     # Return 0 to include the dependent module(s) in the initramfs.
     return 0
 
