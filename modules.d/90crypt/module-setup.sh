f5656e7affad4b6651c7eedb1fe61da845df6cfc
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-05-04 09:00:58 -0400

    perf(crypt): move more rules to systemd-cryptsetup
    In its current form, fido2, pkcs11, tpm2-tss dracut modules
    depend on systemd.

--- modules.d/90crypt/module-setup.sh
+++ modules.d/90crypt/module-setup.sh
@@ -18,21 +18,7 @@ check() {
 
 # called by dracut
 depends() {
-    local deps
-    deps="dm rootfs-block"
-    if [[ $hostonly && -f "$dracutsysrootdir"/etc/crypttab ]]; then
-        if grep -q -e "fido2-device=" -e "fido2-cid=" "$dracutsysrootdir"/etc/crypttab; then
-            deps+=" fido2"
-        fi
-        if grep -q "pkcs11-uri" "$dracutsysrootdir"/etc/crypttab; then
-            deps+=" pkcs11"
-        fi
-        if grep -q "tpm2-device=" "$dracutsysrootdir"/etc/crypttab; then
-            deps+=" tpm2-tss"
-        fi
-    fi
-    echo "$deps"
-    return 0
+    echo dm rootfs-block
 }
 
 # called by dracut
