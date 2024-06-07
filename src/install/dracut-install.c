dracutdevs/dracut

commit d010fa0d7f8ef42ad31729d027d2e4be6dd6e588

refactor(dracut-install): fork() instead of popen(), sanitise line reading
Also set LC_ALL=C to fix ldd output pattern matching in non-english locales.

src/install/dracut-install.c
@@ -498,12 +498,10 @@ static char *get_real_file(const char *src, bool fullyresolve)
 
 static int resolve_deps(const char *src)
 {
-        int ret = 0;
+        int ret = 0, err;
 
         _cleanup_free_ char *buf = NULL;
-        size_t linesize = LINE_MAX;
-        _cleanup_pclose_ FILE *fptr = NULL;
-        _cleanup_free_ char *cmd = NULL;
+        size_t linesize = LINE_MAX + 1;
         _cleanup_free_ char *fullsrcpath = NULL;
 
         fullsrcpath = get_real_file(src, true);
@@ -511,7 +509,7 @@ static int resolve_deps(const char *src)
         if (!fullsrcpath)
                 return 0;
 
-        buf = malloc0(LINE_MAX);
+        buf = malloc(linesize);
         if (buf == NULL)
                 return -errno;
 
@@ -521,11 +519,11 @@ static int resolve_deps(const char *src)
                 if (fd < 0)
                         return -errno;
 
-                ret = read(fd, buf, LINE_MAX);
+                ret = read(fd, buf, linesize - 1);
                 if (ret == -1)
                         return -errno;
 
-                buf[LINE_MAX - 1] = '\0';
+                buf[ret] = '\0';
                 if (buf[0] == '#' && buf[1] == '!') {
                         /* we have a shebang */
                         char *p, *q;
@@ -540,25 +538,28 @@ static int resolve_deps(const char *src)
                 }
         }
 
-        /* run ldd */
-        _asprintf(&cmd, "%s %s 2>&1", ldd, fullsrcpath);
-
-        log_debug("%s", cmd);
-
-        ret = 0;
-
-        fptr = popen(cmd, "r");
-        if (fptr == NULL) {
-                log_error("Error '%s' initiating pipe stream from '%s'", strerror(errno), cmd);
+        int fds[2];
+        FILE *fptr;
+        if (pipe2(fds, O_CLOEXEC) == -1 || (fptr = fdopen(fds[0], "r")) == NULL) {
+                log_error("Error '%m' initiating pipe stream for %s", ldd);
                 exit(EXIT_FAILURE);
         }
 
-        while (!feof(fptr)) {
-                char *p;
+        log_debug("%s %s", ldd, fullsrcpath);
+        pid_t ldd_pid;
+        if ((ldd_pid = fork()) == 0) {
+                dup2(fds[1], 1);
+                dup2(fds[1], 2);
+                putenv("LC_ALL=C");
+                execlp(ldd, ldd, fullsrcpath, (char *)NULL);
+                _exit(errno == ENOENT ? 127 : 126);
+        }
+        close(fds[1]);
 
-                memset(buf, 0, LINE_MAX);
-                if (getline(&buf, &linesize, fptr) <= 0)
-                        continue;
+        ret = 0;
+
+        while (getline(&buf, &linesize, fptr) >= 0) {
+                char *p;
 
                 log_debug("ldd: '%s'", buf);
 
@@ -569,7 +570,7 @@ static int resolve_deps(const char *src)
                 }
 
                 /* errors from cross-compiler-ldd */
-                if (strstr(buf, "unable to find sysroot") || strstr(buf, "command not found")) {
+                if (strstr(buf, "unable to find sysroot")) {
                         log_error("%s", buf);
                         ret += 1;
                         break;
@@ -621,7 +622,21 @@ static int resolve_deps(const char *src)
                 }
         }
 
-        return ret;
+        fclose(fptr);
+        while (waitpid(ldd_pid, &err, 0) == -1) {
+                if (errno != EINTR) {
+                        log_error("ERROR: waitpid() failed: %m");
+                        return 1;
+                }
+        }
+        err = WIFSIGNALED(err) ? 128 + WTERMSIG(err) : WEXITSTATUS(err);
+        /* ldd has error conditions we largely don't care about ("not a dynamic executable", &c.):
+           only error out on hard errors (ENOENT, ENOEXEC, signals) */
+        if (err >= 126) {
+                log_error("ERROR: '%s %s' failed with %d", ldd, fullsrcpath, err);
+                return err;
+        } else
+                return ret;
 }
 
 /* Install ".<filename>.hmac" file for FIPS self-checks */

dracut-ng/dracut-ng

commit 38dea7dd671fd621b563377cfbd95e4783568c6e

feat(dracut-install): split ldd command arguments for execvp()
This restores a not-so-subtle feature of previously executing ldd
via popen(), i.e. the ability to use a cross-compiled sysroot.

The ldd command may be passed in via the DRACUT_LDD environment
variable, and the command may contain command line arguments.
The number of such arguments are not known in advance.

Split the command into executable and arguments and run it
via execvp().

Fixes: d010fa0d7f8ef42ad31729d027d2e4be6dd6e588
Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

src/install/dracut-install.c
@@ -559,10 +559,55 @@ static int resolve_deps(const char *src)
         log_debug("%s %s", ldd, fullsrcpath);
         pid_t ldd_pid;
         if ((ldd_pid = fork()) == 0) {
+                char **cmdline = NULL;
+                int start, pos, idx = 0;
+                /* Account for at least 2 elements plus the terminating NULL in cmdline */
+                int args = 3;
+
+                /* Estimate the number of space-separated elements in the "ldd" string */
+                pos = 0;
+                while (isspace(ldd[pos]))
+                        pos++;
+                for (; ldd[pos]; pos++) {
+                        if (isspace(ldd[pos])) {
+                                if (pos)
+                                        args++;
+                                while (isspace(ldd[pos]))
+                                        pos++;
+                        }
+                }
+
+                cmdline = malloc(args * sizeof(char *));
+                memset(cmdline, 0, args * sizeof(char *));
+
+                pos = 0;
+                while (isspace(ldd[pos]))
+                        pos++;
+                start = pos;
+                for (; ldd[pos]; pos++) {
+                        while (ldd[pos] && !isspace(ldd[pos]))
+                                pos++;
+
+                        cmdline[idx] = malloc(pos - start + 1);
+                        memcpy(cmdline[idx], ldd + start, pos - start);
+                        cmdline[idx][pos - start] = 0;
+                        idx++;
+
+                        if (!ldd[pos])
+                                break;
+
+                        while (isspace(ldd[pos]))
+                                pos++;
+                        start = pos;
+                }
+
+                cmdline[idx++] = fullsrcpath;
+                cmdline[idx] = NULL;
+
                 dup2(fds[1], 1);
                 dup2(fds[1], 2);
                 putenv("LC_ALL=C");
-                execlp(ldd, ldd, fullsrcpath, (char *)NULL);
+                execvp(cmdline[0], cmdline);
                 _exit(errno == ENOENT ? 127 : 126);
         }
         close(fds[1]);