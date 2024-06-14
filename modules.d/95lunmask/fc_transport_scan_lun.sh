e27c2b51ea7e27c0e95d479d2c9c3b4a1a9f495f
Benjamin Drung <benjamin.drung@canonical.com>
2024-06-14 23:49:13 +0200

    fix(lunmask): quote LUN variable
    shellcheck complains about SC2086 (info): Double quote to prevent
    globbing and word splitting.
    
    The variable `LUN` refers to a single logical unit number (LUN) and
    therefore is safe to be quoted.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

--- modules.d/95lunmask/fc_transport_scan_lun.sh
+++ modules.d/95lunmask/fc_transport_scan_lun.sh
@@ -23,4 +23,4 @@ if [ -f /sys"$DEVPATH"/scsi_target_id ]; then
     read -r TARGET < /sys"$DEVPATH"/scsi_target_id
 fi
 [ -z "$TARGET" ] && exit 1
-echo "$CHANNEL" "$TARGET" $LUN > /sys/class/scsi_host/host"$HOST"/scan
+echo "$CHANNEL $TARGET $LUN" > /sys/class/scsi_host/host"$HOST"/scan
