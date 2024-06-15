42bb6e53a59ea55dfb7329d88c725c659fdbcd51
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-06-15 15:21:44 -0400

    fix(crypt): decryption when rd.luks.name is set

--- modules.d/90crypt/parse-crypt.sh
+++ modules.d/90crypt/parse-crypt.sh
@@ -174,7 +174,7 @@ else
                 } >> "$hookdir/emergency/90-crypt.sh"
             fi
         done
-    elif getargbool 1 rd.auto; then
+    elif getargbool 1 rd.auto && [ -z "$(getargs rd.luks.name)" ]; then
         if [ -z "$DRACUT_SYSTEMD" ]; then
             {
                 printf -- 'ENV{ID_FS_TYPE}=="crypto_LUKS", RUN+="%s ' "$(command -v initqueue)"
