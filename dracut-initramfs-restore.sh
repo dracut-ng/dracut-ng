dracut-ng/dracut-ng

commit 28820e20532890b9b0cf816ce1b527c582a1f3b3

feat(dracut.sh): make initramfs-${kernel}.img filename configurable

dracut-initramfs-restore.sh
@@ -46,8 +46,15 @@ elif mountpoint -q /efi; then
 elif mountpoint -q /boot/efi; then
     IMG="/boot/efi/$MACHINE_ID/$KERNEL_VERSION/initrd"
 else
-    echo "No initramfs image found to restore!"
-    exit 1
+    files=("/boot/initr*${KERNEL_VERSION}*")
+    if [ "${#files[@]}" -ge 1 ] && [ -e "${files[0]}" ]; then
+        IMG="${files[0]}"
+    elif [[ -f /boot/initramfs-linux.img ]]; then
+        IMG="/boot/initramfs-linux.img"
+    else
+        echo "No initramfs image found to restore!"
+        exit 1
+    fi
 fi
 
 cd /run/initramfs

dracut.sh
@@ -1138,6 +1138,9 @@ if ! [[ $outfile ]]; then
         mkdir -p "$dracutsysrootdir$efidir/Linux"
         outfile="$dracutsysrootdir$efidir/Linux/linux-$kernel${MACHINE_ID:+-${MACHINE_ID}}${BUILD_ID:+-${BUILD_ID}}.efi"
     else
+        if ! [[ $initrdname ]]; then
+            initrdname="initramfs-${kernel}.img"
+        fi
         if [[ -d "$dracutsysrootdir"/efi/loader/entries || -L "$dracutsysrootdir"/efi/loader/entries ]] \
             && [[ $MACHINE_ID ]] \
             && [[ -d "$dracutsysrootdir"/efi/${MACHINE_ID} || -L "$dracutsysrootdir"/efi/${MACHINE_ID} ]]; then
@@ -1153,7 +1156,7 @@ if ! [[ $outfile ]]; then
         elif [[ -f "$dracutsysrootdir"/lib/modules/${kernel}/initrd ]]; then
             outfile="$dracutsysrootdir/lib/modules/${kernel}/initrd"
         elif [[ -e $dracutsysrootdir/boot/vmlinuz-${kernel} || -e $dracutsysrootdir/boot/vmlinux-${kernel} ]]; then
-            outfile="$dracutsysrootdir/boot/initramfs-${kernel}.img"
+            outfile="$dracutsysrootdir/boot/$initrdname"
         elif [[ -z $dracutsysrootdir ]] \
             && [[ $MACHINE_ID ]] \
             && mountpoint -q /efi; then
@@ -1163,7 +1166,7 @@ if ! [[ $outfile ]]; then
             && mountpoint -q /boot/efi; then
             outfile="/boot/efi/${MACHINE_ID}/${kernel}/initrd"
         else
-            outfile="$dracutsysrootdir/boot/initramfs-${kernel}.img"
+            outfile="$dracutsysrootdir/boot/$initrdname"
         fi
     fi
 fi

man/dracut.8.asc
@@ -18,13 +18,8 @@ DESCRIPTION
 
 Create an initramfs <image> for the kernel with the version <kernel version>.
 If <kernel version> is omitted, then the version of the actual running
-kernel is used. If <image> is omitted or empty, depending on bootloader
-specification, the default location can be
-_/efi/<machine-id>/<kernel-version>/initrd_,
-_/boot/<machine-id>/<kernel-version>/initrd_,
-_/boot/efi/<machine-id>/<kernel-version>/initrd_,
-_/lib/modules/<kernel-version>/initrd_ or
-_/boot/initramfs-<kernel-version>.img_.
+kernel is used. If <image> is omitted or empty, the default location will be
+determined by the local configuration or Linux distribution policy.
 
 dracut creates an initial image used by the kernel for preloading the block
 device modules (such as IDE, SCSI or RAID) which are needed to access the root

man/dracut.conf.5.asc
@@ -320,6 +320,12 @@ Logging levels:
    If set to _yes_, try to execute tasks in parallel (currently only supported
    for _--regenerate-all_).
 
+*initrdname=*"_<filepattern>_"::
+    Specifies the file name for the generated initramfs if it is not set otherwise.
+    The initrdname configuration option is required to match the _initr*${kernel}*_
+    file pattern and only one file with this pattern should exists in the
+    directory where initramfs is loaded from. Defaults to _initramfs-${kernel}.img_.
+
 Files
 -----
 _/etc/dracut.conf_::

man/dracut.usage.asc
@@ -5,13 +5,10 @@ To create a initramfs image, the most simple command is:
 
 This will generate a general purpose initramfs image, with all possible
 functionality resulting of the combination of the installed dracut modules and
-system tools. The image, depending on bootloader specification, can be
-_/efi/_++<machine-id>++_/_++<kernel-version>++_/initrd_,
-_/boot/_++<machine-id>++_/_++<kernel-version>++_/initrd_,
-_/boot/efi/_++<machine-id>++_/_++<kernel-version>++_/initrd_,
-_/lib/modules/_++<kernel-version>++_/initrd_ or
-_/boot/initramfs-_++<kernel-version>++_.img_ and contains the kernel modules of
+system tools. The image contains the kernel modules of
 the currently active kernel with version _++<kernel-version>++_.
+The default location of the image is determined by the local configuration
+or Linux distribution policy.
 
 If the initramfs image already exists, dracut will display an error message, and
 to overwrite the existing image, you have to use the --force option.

man/lsinitrd.1.asc
@@ -18,11 +18,8 @@ SYNOPSIS
 DESCRIPTION
 -----------
 lsinitrd shows the contents of an initramfs image. if <image> is omitted, then
-lsinitrd uses the default image _/efi/<machine-id>/<kernel-version>/initrd_,
-_/boot/<machine-id>/<kernel-version>/initrd_,
-_/boot/efi/<machine-id>/<kernel-version>/initrd_,
-_/lib/modules/<kernel-version>/initrd_ or
-_/boot/initramfs-<kernel-version>.img_.
+lsinitrd determines the default location based on the local configuration
+or Linux distribution policy.
 
 OPTIONS
 -------

commit c4c218ae1fd31c4e05d5f6e6a6c4220e8c4d44d3

fix(dracut-initramfs-restore.sh): correct initrd globbing
Quoting the globbing `*` will prevent Bash from use globbing. So only
quote the kernel version variable that could potentially contain spaces.

Fixes: 28820e205328 ("feat(dracut.sh): make initramfs-${kernel}.img filename configurable")
Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

dracut-initramfs-restore.sh
@@ -46,7 +46,7 @@ elif mountpoint -q /efi; then
 elif mountpoint -q /boot/efi; then
     IMG="/boot/efi/$MACHINE_ID/$KERNEL_VERSION/initrd"
 else
-    files=("/boot/initr*${KERNEL_VERSION}*")
+    files=(/boot/initr*"${KERNEL_VERSION}"*)
     if [ "${#files[@]}" -ge 1 ] && [ -e "${files[0]}" ]; then
         IMG="${files[0]}"
     elif [[ -f /boot/initramfs-linux.img ]]; then