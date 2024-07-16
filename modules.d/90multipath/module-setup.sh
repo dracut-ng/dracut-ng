dracut-ng/dracut-ng

commit 377d52cb9867dc4074fe78965e1bfcdf80be14a1

fix(multipath): omit module if included with no multipath devices
Follow-up to 4957ffa935. return 255 instead of 1 and change the order
of checks.

modules.d/90multipath/module-setup.sh
@@ -23,6 +23,10 @@ majmin_to_mpath_dev() {
 check() {
     local _any_mpath_dev
 
+    # if there's no multipath binary, no go.
+    require_binaries multipath || return 1
+    require_binaries kpartx || return 1
+
     for_each_host_dev_and_slaves is_mpath
     _any_mpath_dev=$?
 
@@ -30,12 +34,8 @@ check() {
         [[ $_any_mpath_dev == 0 ]] || return 255
     }
 
-    # if there's no multipath binary, no go.
-    require_binaries multipath || return 1
-    require_binaries kpartx || return 1
-
     if [[ $_any_mpath_dev != 0 ]] && [[ ! -f /etc/multipath.conf ]]; then
-        return 1
+        return 255
     fi
 
     return 0

commit 3560bc5b2cb215654596745f60ed18a1815035a0

fix(multipath): include module with "find_multipaths strict"
The previous commits 377d52c and 4957ffa excluded the multipath module in
non-hostonly mode, with the purpose of avoiding to start multipath in the
initramfs on systems that don't use multipath. This is against the idea of
non-hostonly, which is to build an initramfs with all kernel modules and tools
that may be necessary to bring up the system.

This patch takes a different approach. In non-hostonly mode, if there are no
multipath devices and no multipath.conf, include the multipath module, but
require "find_multipaths strict" policy. This will ensure that users that don't
use dm-multipath won't be surprised by finding their root device under
multipath control. But the multipath tools will still be included in the
initramfs, allowing users to set up multipathing e.g. on the emergency shell if
necessary. Also, if /etc/multipath/wwids exists, it will be included in the
initramfs and will cause the WWIDs listed in that file to be multipathed.

Note that in multipath-tools upstream and many Linux distributions,
"find_multipaths strict" is the default anyway. Users of distributions with
different defaults (e.g. openSUSE) would just need to create
/etc/multipath.conf (possibly with empty contents) to override this behavior if
they intend to activate multipath in the initramfs.

Fixes: 377d52c ("fix(multipath): omit module if included with no multipath devices")
Fixes: 4957ffa ("fix(multipath): omit module if included with no multipath devices")

modules.d/90multipath/module-setup.sh
@@ -21,22 +21,13 @@ majmin_to_mpath_dev() {
 
 # called by dracut
 check() {
-    local _any_mpath_dev
-
     # if there's no multipath binary, no go.
     require_binaries multipath || return 1
     require_binaries kpartx || return 1
 
-    for_each_host_dev_and_slaves is_mpath
-    _any_mpath_dev=$?
-
     [[ $hostonly ]] || [[ $mount_needs ]] && {
-        [[ $_any_mpath_dev == 0 ]] || return 255
+        for_each_host_dev_and_slaves is_mpath || return 255
     }
-
-    if [[ $_any_mpath_dev != 0 ]] && [[ ! -f /etc/multipath.conf ]]; then
-        return 255
-    fi
 
     return 0
 }
@@ -129,6 +120,19 @@ install() {
             done
             mpathconf "${_args[@]}" --outfile "${initdir}"/etc/multipath.conf
         fi
+    }
+
+    [[ $hostonly ]] || {
+        for_each_host_dev_and_slaves is_mpath \
+            || [[ -f /etc/multipath.conf ]] || {
+            cat > "${initdir}"/etc/multipath.conf << EOF
+# Minimal multipath.conf generated by dracut
+# Avoid any devices being multipathed by default
+defaults {
+         find_multipaths strict
+}
+EOF
+        }
     }
 
     inst "$(command -v partx)" /sbin/partx