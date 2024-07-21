3975e26a84cd8645b15d0a863263a63da93650e0
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-07-20 20:24:13 -0400

    feat(busybox): use busybox --install to install itself
    Previous commits in this PR exposed a bug whereby some utilities
    (e.g. dd) had both a busybox applet version and a coreutils version.
    
    The busybox dracut module should not enforce taking always the
    busybox version. The `busybox --install` command is safer option
    that does not overwrite an existing binary of already exists.

--- modules.d/05busybox/module-setup.sh
+++ modules.d/05busybox/module-setup.sh
@@ -14,18 +14,8 @@ depends() {
 
 # called by dracut
 install() {
-    local _i _path _busybox
-    local _progs=()
+    local _busybox
     _busybox=$(find_binary busybox)
     inst "$_busybox" /usr/bin/busybox
-    for _i in $($_busybox --list); do
-        [[ ${_i} == busybox ]] && continue
-        _progs+=("${_i}")
-    done
-
-    for _i in "${_progs[@]}"; do
-        _path=$(find_binary "$_i")
-        [ -z "$_path" ] && continue
-        ln_r /usr/bin/busybox "$_path"
-    done
+    $_busybox --install -s /bin/
 }
