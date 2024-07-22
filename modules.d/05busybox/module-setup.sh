95ba03270eb3297103cba276c2925ecc1b762926
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-07-22 07:50:34 -0400

    fix(busybox): install busybox symlinks manually
    Do not depend on CONFIG_FEATURE_INSTALLER.
    Do not remove existing destination files.

--- modules.d/05busybox/module-setup.sh
+++ modules.d/05busybox/module-setup.sh
@@ -14,8 +14,25 @@ depends() {
 
 # called by dracut
 install() {
-    local _busybox
+    local _i _path _busybox
+    local _progs=()
     _busybox=$(find_binary busybox)
     inst "$_busybox" /usr/bin/busybox
-    $_busybox --install -s /bin/
+
+    # do not depend on CONFIG_FEATURE_INSTALLER
+    # install busybox symlinks manually
+    for _i in $($_busybox --list); do
+        [[ ${_i} == busybox ]] && continue
+        _progs+=("${_i}")
+    done
+
+    for _i in "${_progs[@]}"; do
+        _path=$(find_binary "$_i")
+        [ -z "$_path" ] && continue
+
+        # do not remove existing destination files
+        [ -e "${initdir}/$_path" ] && continue
+
+        ln_r /usr/bin/busybox "$_path"
+    done
 }
