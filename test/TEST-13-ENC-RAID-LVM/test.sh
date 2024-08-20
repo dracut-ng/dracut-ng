33559bb892a136989c7fb32e98cc81454937a1d3
Jo Zzsi <jozzsicsataban@gmail.com>
2024-08-19 20:22:54 -0400

    test: improve tests with crypt dracut module to work in hostonly mode
    Enable running these test both in hostonly and non-hostonly mode.

--- test/TEST-13-ENC-RAID-LVM/test.sh
+++ test/TEST-13-ENC-RAID-LVM/test.sh
@@ -98,7 +98,7 @@ test_setup() {
     chmod 0600 /tmp/key
 
     test_dracut \
-        --no-hostonly \
+        -a "crypt lvm mdraid" \
         -i "./cryptroot-ask.sh" "/sbin/cryptroot-ask" \
         -i "/tmp/crypttab" "/etc/crypttab" \
         -i "/tmp/key" "/etc/key" \
