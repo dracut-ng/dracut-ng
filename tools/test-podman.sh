712d31b6448c5122d2ba502111c24741745161aa
Jo Zzsi <jozzsicsataban@gmail.com>
2024-08-23 22:47:29 -0400

    test(test-podman.sh): script to run tests in podman

---
+++ tools/test-podman.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+
+TESTS=$1
+CONTAINER=$2
+
+[ -z "$CONTAINER" ] && CONTAINER='ghcr.io/dracut-ng/fedora'
+
+podman run --rm -it --device=/dev/kvm -v "$PWD"/:/z "$CONTAINER" /z/tools/test-github.sh id "$TESTS"
