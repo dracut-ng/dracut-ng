6ff40fc639ecdeef234328e4d97fdb288efc669c
Jo Zzsi <jozzsicsataban@gmail.com>
2024-09-13 21:39:14 -0400

    chore(systemd): move steps into more specific dracut modules

--- modules.d/01systemd-udevd/module-setup.sh
+++ modules.d/01systemd-udevd/module-setup.sh
@@ -73,6 +73,12 @@ install() {
         fi
     fi
 
+    inst_binary true
+    ln_r "$(find_binary true)" "/usr/bin/loginctl"
+    ln_r "$(find_binary true)" "/bin/loginctl"
+    inst_rules \
+        99-systemd.rules
+
     # Install required libraries.
     _arch=${DRACUT_ARCH:-$(uname -m)}
     inst_libdir_file {"tls/$_arch/",tls/,"$_arch/",}"libudev.so.*"
