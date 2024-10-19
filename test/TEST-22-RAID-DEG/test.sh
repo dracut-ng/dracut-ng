18f0255b76b4ad21572060382053af906d1f29f6
Jo Zzsi <jozzsicsataban@gmail.com>
2024-10-19 09:48:55 -0400

    test(RAID-DEG): reenable systemd for this test

--- test/TEST-22-RAID-DEG/test.sh
+++ test/TEST-22-RAID-DEG/test.sh
@@ -97,7 +97,8 @@ test_setup() {
     chmod 0600 /tmp/key
 
     test_dracut \
-        -m "crypt lvm mdraid kernel-modules" \
+        -a "crypt lvm mdraid kernel-modules" \
+        -o network \
         -i "./cryptroot-ask.sh" "/sbin/cryptroot-ask" \
         -i "/tmp/mdadm.conf" "/etc/mdadm.conf" \
         -i "/tmp/crypttab" "/etc/crypttab" \
