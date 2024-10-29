6dd0de69e960be1ddfbc770d1a222e7b8ffaacd0
Jo Zzsi <jozzsicsataban@gmail.com>
2024-10-29 09:31:44 -0400

    ci: add enviroment variable for make target

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# convenience script for development to run integration tests
+# runs test-github.sh in a container
+
 set -e
 
-CONTAINER=$1
-TESTS=$2
-
-[ -z "$CONTAINER" ] && CONTAINER='fedora'
+export CONTAINER="${CONTAINER:=$1}"
+export CONTAINER="${CONTAINER:=fedora}"
+export TESTS="${TESTS:=$2}"
+export TEST_RUN_ID="${TEST_RUN_ID:=id}"
 
 if command -v podman &> /dev/null; then
-    podman run --rm -it --device=/dev/kvm -e V=0 -v "$PWD"/:/z "ghcr.io/dracut-ng/$CONTAINER" /z/test/test-github.sh id "$TESTS"
+    PODMAN=podman
 else
-    docker run --rm -it --device=/dev/kvm -e V=0 -v "$PWD"/:/z "ghcr.io/dracut-ng/$CONTAINER" /z/test/test-github.sh id "$TESTS"
+    PODMAN=docker
 fi
+
+# clear previous test run
+TARGETS='clean all check' "$PODMAN" run --rm -it \
+    --device=/dev/kvm \
+    -e V -e TESTS -e TEST_RUN_ID -e TARGETS \
+    -v "$PWD"/:/z \
+    "ghcr.io/dracut-ng/$CONTAINER" \
+    /z/test/test-github.sh
