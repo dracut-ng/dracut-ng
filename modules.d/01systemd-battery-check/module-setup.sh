7cf47b26fe2e8a1a6fcac301db74ae3dbbf523b0
Jo Zzsi <jozzsicsataban@gmail.com>
2024-11-09 15:30:05 -0500

    feat: systemd-battery-check dracut module

---
+++ modules.d/01systemd-battery-check/module-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This file is part of dracut.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Prerequisite check(s) for module.
+check() {
+    # If the binary(s) requirements are not fulfilled the module can't be installed
+    require_binaries "$systemdutildir"/systemd-battery-check || return 1
+
+    # Return 255 to only include the module, if another module requires it.
+    return 255
+}
+
+# Module dependency requirements.
+depends() {
+    # This module has external dependency on other module(s).
+    echo systemd
+    # Return 0 to include the dependent module(s) in the initramfs.
+    return 0
+}
+
+# Install the required file(s) for the module in the initramfs.
+install() {
+    inst_multiple \
+        "$systemdsystemunitdir"/systemd-battery-check.service \
+        "$systemdsystemunitdir"/initrd.target.wants/systemd-battery-check.service \
+        "$systemdutildir"/systemd-battery-check
+}
