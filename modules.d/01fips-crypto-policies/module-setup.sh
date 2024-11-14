1ef60f9fcfb57f79532604c1ea1920ace7c21e99
Jo Zzsi <jozzsicsataban@gmail.com>
2024-11-14 07:22:47 -0500

    fix(fips-crypto-policies): improve check for module inclusion
    Checking files under /etc in non-hostonly (generic mode) is
    not recommended. Test files under /usr instead.

--- modules.d/01fips-crypto-policies/module-setup.sh
+++ modules.d/01fips-crypto-policies/module-setup.sh
@@ -3,7 +3,7 @@
 # called by dracut
 check() {
     # only enable on systems that use crypto-policies
-    [ -d "$dracutsysrootdir/etc/crypto-policies" ] && return 0
+    [ -f "$dracutsysrootdir/usr/share/crypto-policies/default-fips-config" ] && return 0
 
     # include when something else depends on it or it is explicitly requested
     return 255
