61e1b53cf2b221668a6b4a91f106f1329253ef67
Jo Zzsi <jozzsicsataban@gmail.com>
2024-11-19 14:32:04 -0500

    test: move wildcard support from test-github.sh to test.sh
    Follow-up to aa967c9 . Since this feature is only used in test.sh
    let's not slow down CI runs with it.

--- test/test-github.sh
+++ test/test-github.sh
@@ -15,15 +15,6 @@ if [ "$V" = "2" ]; then set -x; fi
 # shellcheck disable=SC2086
 ./configure $CONFIGURE_ARG
 
-TESTS="${TESTS:=$2}"
-
-# Compute wildcards for TESTS variable (e.g. '1*')
-# shellcheck disable=SC1001
-[ -n "$TESTS" ] && TESTS=$(
-    cd test
-    for T in ${TESTS}; do find . -depth -type d -name "TEST-*${T}*" -exec echo {} \; | cut -d\- -f2 | tr '\n' ' '; done
-)
-
 # treat warnings as error
 # shellcheck disable=SC2086
-CFLAGS="-Wextra -Werror" make TEST_RUN_ID="${TEST_RUN_ID:=$1}" TESTS="${TESTS}" V="${V:=1}" $MAKEFLAGS ${TARGETS:=all install check}
+CFLAGS="-Wextra -Werror" make TEST_RUN_ID="${TEST_RUN_ID:=$1}" TESTS="${TESTS:=$2}" V="${V:=1}" $MAKEFLAGS ${TARGETS:=all install check}
