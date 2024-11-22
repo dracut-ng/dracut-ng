87fefd3cab7f9c611dc74a7ebc230228a7682e84
Benjamin Drung <benjamin.drung@canonical.com>
2024-11-22 03:58:56 +0100

    fix(iscsi): do not install services when not using systemd
    The iscsid and iscsiuio services are installed by an `inst_multiple` two
    times. Remove installing these service files when not using systemd.
    
    Fixes: b31f3fe0d1be ("95iscsi: Replace iscsistart with iscsid")

--- modules.d/95iscsi/module-setup.sh
+++ modules.d/95iscsi/module-setup.sh
@@ -191,17 +191,6 @@ install() {
     inst_multiple umount iscsi-iname iscsiadm iscsid
     inst_binary sort
 
-    inst_multiple -o \
-        "$systemdsystemunitdir"/iscsid.socket \
-        "$systemdsystemunitdir"/iscsid.service \
-        "$systemdsystemunitdir"/iscsiuio.service \
-        "$systemdsystemunitdir"/iscsiuio.socket \
-        "$systemdsystemunitdir"/sockets.target.wants/iscsid.socket \
-        "$systemdsystemunitdir"/sockets.target.wants/iscsiuio.socket
-    if grep -q '^ExecStartPre=/usr/lib/open-iscsi/startup-checks.sh$' "$systemdsystemunitdir/iscsid.service"; then
-        inst_simple /usr/lib/open-iscsi/startup-checks.sh
-    fi
-
     inst_simple /etc/iscsi/iscsid.conf
     if [[ $hostonly ]]; then
         inst_simple /etc/iscsi/initiatorname.iscsi
@@ -227,7 +216,12 @@ install() {
             "$systemdsystemunitdir"/iscsid.service \
             "$systemdsystemunitdir"/iscsid.socket \
             "$systemdsystemunitdir"/iscsiuio.service \
-            "$systemdsystemunitdir"/iscsiuio.socket
+            "$systemdsystemunitdir"/iscsiuio.socket \
+            "$systemdsystemunitdir"/sockets.target.wants/iscsid.socket \
+            "$systemdsystemunitdir"/sockets.target.wants/iscsiuio.socket
+        if grep -q '^ExecStartPre=/usr/lib/open-iscsi/startup-checks.sh$' "$systemdsystemunitdir/iscsid.service"; then
+            inst_simple /usr/lib/open-iscsi/startup-checks.sh
+        fi
 
         for i in \
             iscsid.socket \
