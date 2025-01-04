f3dacc013d90bd2c0bbfa04f5f9b167b65298440
Jo Zzsi <jozzsicsataban@gmail.com>
2025-01-04 18:45:32 -0500

    feat(systemd-sysusers): run systemd-sysusers as part of the build process
    This PR makes the boot process faster and the generated initrd smaller.
    
    It also make the code easier to maintain.
    
    The primary goal of this PR is refactoring and removing code,
    this is why it is not marked as perf() in the commit.
    
    That said, with this PR systemd-sysusers no longer needs to be copied
    into the initrd, so that is about 50KB saving both on when initrd gets
    saved/compressed and when it gets loaded and uncompressed.
    
    This PR also make the debugging slightly easier as one can just
    inspect /etc/passwd with lsinitrd instead of trying to figure out
    which users are created runtime.

---
+++ modules.d/60systemd-sysusers/module-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This file is part of dracut.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Prerequisite check(s) for module.
+check() {
+    # If the binary(s) requirements are not fulfilled the module can't be installed.
+    require_binaries systemd-sysusers || return 1
+
+    # Return 255 to only include the module, if another module requires it.
+    return 255
+}
+
+# Install the required file(s) and directories for the module in the initramfs.
+install() {
+    inst_sysusers basic.conf
+
+    systemd-sysusers --root="$initdir"
+}
