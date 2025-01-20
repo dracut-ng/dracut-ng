96bba1e5156e5d0cb90d7a1718781dc29b1979c2
Jo Zzsi <jozzsicsataban@gmail.com>
2025-01-19 20:28:06 -0500

    ci: adapt the test.sh script to the new container labeling
    Add architecture tag, see commit d8ff139 .
    
    Document that the script now supports arm64.

--- test/test.sh
+++ test/test.sh
@@ -10,6 +10,29 @@ export CONTAINER="${CONTAINER:=fedora}"
 export TESTS="${TESTS:=$2}"
 export TEST_RUN_ID="${TEST_RUN_ID:=id}"
 
+# if registry is not specified, add our registry
+if [[ $CONTAINER != *"/"* ]]; then
+    CONTAINER="ghcr.io/dracut-ng/$CONTAINER"
+fi
+
+# if label is not specified, add latest label
+if [[ $CONTAINER != *":"* ]]; then
+    CONTAINER="$CONTAINER:latest"
+fi
+
+# add architecture tag, see commit d8ff139
+ARCH="${ARCH-$(uname -m)}"
+case "$ARCH" in
+    aarch64 | arm64)
+        CONTAINER="$CONTAINER-arm"
+        ;;
+    amd64 | i?86 | x86_64)
+        CONTAINER="$CONTAINER-amd"
+        ;;
+esac
+
+echo "Running in a container from $CONTAINER"
+
 if command -v podman &> /dev/null; then
     PODMAN=podman
 else
@@ -28,5 +51,5 @@ TARGETS='clean all install check' "$PODMAN" run --rm -it \
     --device=/dev/kvm --privileged \
     -e V -e TESTS -e TEST_RUN_ID -e TARGETS -e MAKEFLAGS \
     -v "$PWD"/:/z \
-    "ghcr.io/dracut-ng/$CONTAINER" \
+    "$CONTAINER" \
     /z/test/test-github.sh
