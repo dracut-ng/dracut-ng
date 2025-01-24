5e6b792f8207b97671827f02c5e134ae699c21ed
Jo Zzsi <jozzsicsataban@gmail.com>
2025-01-23 19:37:53 -0500

    ci: restore regression for console messages
    BASIC, UEFI, DMSQUASH tests did not had some of the
    kernel command line options properly set

--- test/TEST-12-UEFI/test.sh
+++ test/TEST-12-UEFI/test.sh
@@ -64,12 +64,12 @@ test_setup() {
         ukify build \
             --linux="$VMLINUZ" \
             --initrd="$TESTDIR"/initramfs.testing \
-            --cmdline='root=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_root' \
+            --cmdline="$TEST_KERNEL_CMDLINE root=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_root" \
             --output="$TESTDIR"/ESP/EFI/BOOT/BOOTX64.efi
     else
         echo "Using dracut to create UKI"
         test_dracut \
-            --kernel-cmdline 'root=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_root' \
+            --kernel-cmdline "$TEST_KERNEL_CMDLINE root=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_root" \
             --drivers 'squashfs' \
             --uefi \
             "$TESTDIR"/ESP/EFI/BOOT/BOOTX64.efi
