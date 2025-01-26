a3e6567f7be7bef5474769d1750f070944910415
Jo Zzsi <jozzsicsataban@gmail.com>
2025-01-25 20:53:55 -0500

    test(KERNEL-INSTALL): enable for all CI container
    Add preconditions, which would skip this test on
    Debian/Ubuntu for now.
    
    Move enviroment variables - such as BOOT_ROOT - out
    from the test specific code to the common test infrastrucutre
    so that it is available for all test cases.

--- test/TEST-43-KERNEL-INSTALL/test.sh
+++ test/TEST-43-KERNEL-INSTALL/test.sh
@@ -3,13 +3,22 @@
 TEST_DESCRIPTION="kernel-install with root filesystem on ext4 filesystem"
 
 test_check() {
-
     if command -v systemd-detect-virt > /dev/null && ! systemd-detect-virt -c &> /dev/null; then
-        echo "This script assumes that it runs inside a CI container."
+        echo "This test assumes that it runs inside a CI container."
         return 1
     fi
 
-    command -v kernel-install &> /dev/null
+    if ! [ -e /usr/lib/modules/"$KVERSION"/vmlinuz ]; then
+        echo "This test assumes that kernel-install can find the kernel at /usr/lib/modules/$KVERSION/vmlinuz"
+        return 1
+    fi
+
+    if ! command -v kernel-install > /dev/null; then
+        echo "This test needs kernel-install to run."
+        return 1
+    fi
+
+    command -v systemctl &> /dev/null
 }
 
 test_run() {
@@ -23,7 +32,7 @@ test_run() {
     "$testdir"/run-qemu \
         "${disk_args[@]}" \
         -append "$TEST_KERNEL_CMDLINE root=LABEL=dracut" \
-        -initrd "$TESTDIR"/initramfs.testing || return 1
+        -initrd "$BOOT_ROOT/$TOKEN/$KVERSION"/initrd || return 1
 
     test_marker_check || return 1
 }
@@ -38,19 +47,15 @@ test_setup() {
     dd if=/dev/zero of="$TESTDIR"/root.img bs=200MiB count=1 status=none && sync
     mkfs.ext4 -q -L dracut -d "$TESTDIR"/dracut.*/initramfs/ "$TESTDIR"/root.img && sync
 
-    export BOOT_ROOT="$TESTDIR"
-    [[ -f /etc/machine-id ]] && read -r TOKEN < /etc/machine-id
-    [[ -z $TOKEN ]] && . /etc/os-release && TOKEN="$ID"
-    mkdir -p "$BOOT_ROOT/$TOKEN/$KVERSION" /run/kernel/
+    mkdir -p /run/kernel
     echo 'initrd_generator=dracut' >> /run/kernel/install.conf
 
     # enable test dracut config
     cp /usr/lib/dracut/test/dracut.conf.d/test/test.conf /usr/lib/dracut/dracut.conf.d/
 
     # using kernell-install to invoke dracut
+    mkdir -p "$BOOT_ROOT/$TOKEN/$KVERSION"
     kernel-install add-all
-
-    mv "$BOOT_ROOT/$TOKEN/$KVERSION"/initrd "$TESTDIR"/initramfs.testing
 }
 
 # shellcheck disable=SC1090
