dracutdevs/dracut

commit 2b125c69cc80baae765a868992003ddd349a7ae9

base/dracut-lib.sh: do not setdebug, if not in initramfs

modules.d/99base/dracut-lib.sh
@@ -325,6 +325,7 @@ splitsep() {
 }
 
 setdebug() {
+    [ -f /etc/initrd-release ] || return
     if [ -z "$RD_DEBUG" ]; then
         if [ -e /proc/cmdline ]; then
             RD_DEBUG=no

dracut-ng/dracut-ng

commit 31b4a6421d00efaf3d2aaa900c1a6153719c2ee3

fix(dracut-lib): support "set -e" in setdebug
A `return` statement will return with the exit code of the previous
command if no exit code is specified. In case `/usr/lib/initrd-release`
does not exist, `setdebug` will return with the exit code 1.

Return this function with code 0 in that case to support `set -e` users.

Fixes: 2b125c69cc80 ("base/dracut-lib.sh: do not setdebug, if not in initramfs")

modules.d/99base/dracut-lib.sh
@@ -340,7 +340,7 @@ splitsep() {
 }
 
 setdebug() {
-    [ -f /usr/lib/initrd-release ] || return
+    [ -f /usr/lib/initrd-release ] || return 0
     if [ -z "$RD_DEBUG" ]; then
         if [ -e /proc/cmdline ]; then
             RD_DEBUG=no