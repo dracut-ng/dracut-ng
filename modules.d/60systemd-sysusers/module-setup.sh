dracut-ng/dracut-ng

commit f3dacc013d90bd2c0bbfa04f5f9b167b65298440

feat(systemd-sysusers): run systemd-sysusers as part of the build process
This PR makes the boot process faster and the generated initrd smaller.

It also make the code easier to maintain.

The primary goal of this PR is refactoring and removing code,
this is why it is not marked as perf() in the commit.

That said, with this PR systemd-sysusers no longer needs to be copied
into the initrd, so that is about 50KB saving both on when initrd gets
saved/compressed and when it gets loaded and uncompressed.

This PR also make the debugging slightly easier as one can just
inspect /etc/passwd with lsinitrd instead of trying to figure out
which users are created runtime.

modules.d/60systemd-sysusers/module-setup.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+# This file is part of dracut.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Prerequisite check(s) for module.
+check() {
+    # If the binary(s) requirements are not fulfilled the module can't be installed.
+    require_binaries systemd-sysusers || return 1
+
+    # Return 255 to only include the module, if another module requires it.
+    return 255
+}
+
+# Install the required file(s) and directories for the module in the initramfs.
+install() {
+    inst_sysusers basic.conf
+
+    systemd-sysusers --root="$initdir"
+}

modules.d/01systemd-sysusers/module-setup.sh
@@ -1,31 +1,0 @@
-#!/bin/bash
-# This file is part of dracut.
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-# Prerequisite check(s) for module.
-check() {
-    # If the binary(s) requirements are not fulfilled the module can't be installed.
-    require_binaries systemd-sysusers || return 1
-
-    # Return 255 to only include the module, if another module requires it.
-    return 255
-}
-
-# Install the required file(s) and directories for the module in the initramfs.
-install() {
-    inst_simple "$moddir/sysusers-dracut.conf" "$systemdsystemunitdir/systemd-sysusers.service.d/sysusers-dracut.conf"
-
-    inst_sysusers basic.conf
-
-    inst_multiple -o \
-        "$systemdsystemunitdir"/systemd-sysusers.service \
-        "$systemdsystemunitdir"/sysinit.target.wants/systemd-sysusers.service \
-        systemd-sysusers
-
-    # Install the hosts local user configurations if enabled.
-    if [[ $hostonly ]]; then
-        inst_multiple -H -o \
-            "$systemdsystemconfdir"/systemd-sysusers.service \
-            "$systemdsystemconfdir/systemd-sysusers.service.d/*.conf"
-    fi
-}

modules.d/01systemd-sysusers/sysusers-dracut.conf
@@ -1,2 +1,0 @@
-[Unit]
-ConditionNeedsUpdate=

commit db92d603619b3dca90224703a413ea03cd61d3c0

fix(systemd-sysusers): silence "Creating " on stderr
dracut prints 20 lines when creating users and groups even with
`--quiet` option. Sample output:

```
Creating group 'nobody' with GID 65534.
Creating group 'audio' with GID 997.
Creating group 'disk' with GID 995.
Creating group 'input' with GID 994.
Creating group 'kmem' with GID 993.
Creating group 'kvm' with GID 992.
Creating group 'lp' with GID 991.
Creating group 'optical' with GID 990.
Creating group 'render' with GID 989.
Creating group 'sgx' with GID 988.
Creating group 'storage' with GID 987.
Creating group 'tty' with GID 5.
Creating group 'uucp' with GID 986.
Creating group 'video' with GID 985.
Creating group 'users' with GID 984.
Creating group 'systemd-journal' with GID 983.
Creating user 'root' (Super User) with UID 0 and GID 0.
Creating user 'nobody' (Kernel Overflow User) with UID 65534 and GID 65534.
Creating group 'nobody' with GID 65534.
Creating group 'audio' with GID 997.
```

Filter "Creating " messages from stderr, but keep the other messages on
stderr and all messages on stdout untouched.

Fixes: https://github.com/dracut-ng/dracut-ng/issues/1195
Fixes: f3dacc013d90 ("feat(systemd-sysusers): run systemd-sysusers as part of the build process")

modules.d/60systemd-sysusers/module-setup.sh
@@ -15,5 +15,9 @@ check() {
 install() {
     inst_sysusers basic.conf
 
-    systemd-sysusers --root="$initdir" > /dev/null
+    # redirect stdout temporarily to FD 3 to use filter stderr
+    {
+        set -o pipefail
+        systemd-sysusers --root="$initdir" 2>&1 >&3 | grep -v "^Creating " >&2
+    } 3>&1
 }