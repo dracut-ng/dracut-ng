ad0fd3a8dfcb155290bc8b7ec9f83144daa3d9df
Jo Zzsi <jozzsicsataban@gmail.com>
2025-05-29 10:06:26 -0400

    perf(watchdog): only install wdctl for the non-systemd case
    Move the installation of wdctl in install() to the non-systemd block as
    systemd doesn't use it, so it's only needed by watchdog.sh.

--- modules.d/04watchdog/module-setup.sh
+++ modules.d/04watchdog/module-setup.sh
@@ -30,8 +30,9 @@ install() {
         inst_hook pre-pivot 99 "$moddir/watchdog.sh"
         inst_hook cleanup 00 "$moddir/watchdog.sh"
         inst_hook cleanup 99 "$moddir/watchdog.sh"
+
+        inst_multiple -o wdctl
     fi
 
     inst_hook emergency 02 "$moddir/watchdog-stop.sh"
-    inst_multiple -o wdctl
 }
