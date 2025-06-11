dracut-ng/dracut-ng

commit 2199846ffebca406a957b781509c42a42d101ba7

fix(modules): all modules with 99 ordering should have a unique number
This PR does not change the ascending sort order of the modules.

Partially resolve https://github.com/dracut-ng/dracut-ng/issues/538#issuecomment-2487284279

Makefile
@@ -37,14 +37,14 @@ man7pages = man/dracut.cmdline.7 \
 
 man8pages = man/dracut.8 \
             man/dracut-catimages.8 \
-            modules.d/98dracut-systemd/dracut-cmdline.service.8 \
-            modules.d/98dracut-systemd/dracut-mount.service.8 \
-            modules.d/98dracut-systemd/dracut-shutdown.service.8 \
-            modules.d/98dracut-systemd/dracut-pre-mount.service.8 \
-            modules.d/98dracut-systemd/dracut-pre-pivot.service.8 \
-            modules.d/98dracut-systemd/dracut-pre-trigger.service.8 \
-            modules.d/98dracut-systemd/dracut-pre-udev.service.8 \
-            modules.d/99initqueue/dracut-initqueue.service.8
+            modules.d/77dracut-systemd/dracut-cmdline.service.8 \
+            modules.d/77dracut-systemd/dracut-mount.service.8 \
+            modules.d/77dracut-systemd/dracut-shutdown.service.8 \
+            modules.d/77dracut-systemd/dracut-pre-mount.service.8 \
+            modules.d/77dracut-systemd/dracut-pre-pivot.service.8 \
+            modules.d/77dracut-systemd/dracut-pre-trigger.service.8 \
+            modules.d/77dracut-systemd/dracut-pre-udev.service.8 \
+            modules.d/77initqueue/dracut-initqueue.service.8
 
 manpages = $(man1pages) $(man5pages) $(man7pages) $(man8pages)
 
@@ -190,8 +190,8 @@ ifneq ($(enable_documentation),no)
 endif
 	if [ -n "$(systemdsystemunitdir)" ]; then \
 		mkdir -p $(DESTDIR)$(systemdsystemunitdir); \
-		ln -srf $(DESTDIR)$(pkglibdir)/modules.d/98dracut-systemd/dracut-shutdown-onfailure.service $(DESTDIR)$(systemdsystemunitdir)/dracut-shutdown-onfailure.service; \
-		ln -srf $(DESTDIR)$(pkglibdir)/modules.d/98dracut-systemd/dracut-shutdown.service $(DESTDIR)$(systemdsystemunitdir)/dracut-shutdown.service; \
+		ln -srf $(DESTDIR)$(pkglibdir)/modules.d/77dracut-systemd/dracut-shutdown-onfailure.service $(DESTDIR)$(systemdsystemunitdir)/dracut-shutdown-onfailure.service; \
+		ln -srf $(DESTDIR)$(pkglibdir)/modules.d/77dracut-systemd/dracut-shutdown.service $(DESTDIR)$(systemdsystemunitdir)/dracut-shutdown.service; \
 		mkdir -p $(DESTDIR)$(systemdsystemunitdir)/sysinit.target.wants; \
 		ln -sf ../dracut-shutdown.service \
 		$(DESTDIR)$(systemdsystemunitdir)/sysinit.target.wants/dracut-shutdown.service; \
@@ -204,11 +204,11 @@ endif
 		    dracut-pre-trigger.service \
 		    dracut-pre-udev.service \
 		    ; do \
-			ln -srf $(DESTDIR)$(pkglibdir)/modules.d/98dracut-systemd/$$i $(DESTDIR)$(systemdsystemunitdir); \
+			ln -srf $(DESTDIR)$(pkglibdir)/modules.d/77dracut-systemd/$$i $(DESTDIR)$(systemdsystemunitdir); \
 			ln -sf ../$$i \
 			$(DESTDIR)$(systemdsystemunitdir)/initrd.target.wants/$$i; \
 		done; \
-		ln -srf $(DESTDIR)$(pkglibdir)/modules.d/99initqueue/dracut-initqueue.service $(DESTDIR)$(systemdsystemunitdir); \
+		ln -srf $(DESTDIR)$(pkglibdir)/modules.d/77initqueue/dracut-initqueue.service $(DESTDIR)$(systemdsystemunitdir); \
 		ln -sf ../dracut-initqueue.service \
 		$(DESTDIR)$(systemdsystemunitdir)/initrd.target.wants/dracut-initqueue.service; \
 	fi

commit 61e632b6b8c5247efb0d877195fce884ea23c938

fix(Makefile): remove test modules after renumbering
The test modules were renumbered from 80 to 70, but the Makefile did not
adjust to this change.

Fixes: 2199846ffebc ("fix(modules): all modules with 99 ordering should have a unique number")

Makefile
@@ -171,7 +171,6 @@ install: all
 	ln -fs dracut-functions.sh $(DESTDIR)$(pkglibdir)/dracut-functions
 	install -m 0755 dracut-logger.sh $(DESTDIR)$(pkglibdir)/dracut-logger.sh
 	install -m 0755 dracut-initramfs-restore.sh $(DESTDIR)$(pkglibdir)/dracut-initramfs-restore
-	rm -rf $(DESTDIR)$(pkglibdir)/modules.d/80test*
 	cp -arx modules.d dracut.conf.d $(DESTDIR)$(pkglibdir)
 	for i in $(configprofile) ; do \
 		cp -arx dracut.conf.d/$$i/* $(DESTDIR)$(pkglibdir)/dracut.conf.d/ ;\
@@ -179,7 +178,7 @@ install: all
 ifneq ($(enable_test),no)
 	cp -arx test $(DESTDIR)$(pkglibdir)
 else
-	rm -rf $(DESTDIR)$(pkglibdir)/modules.d/80test*
+	rm -rf $(DESTDIR)$(pkglibdir)/modules.d/70test*
 endif
 ifneq ($(enable_documentation),no)
 	for i in $(man1pages); do install -m 0644 $$i $(DESTDIR)$(mandir)/man1/$${i##*/}; done