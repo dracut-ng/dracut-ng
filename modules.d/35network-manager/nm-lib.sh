630ab2c4a4a536287bd629051460b61b546547dc
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2025-06-13 09:30:11 +0200

    feat(network-manager): use upstream initrd services if available
    Added in https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/commit/8640ea822f054c8817e0fd773ba7d1a8f42fa214

--- modules.d/35network-manager/nm-lib.sh
+++ modules.d/35network-manager/nm-lib.sh
@@ -3,31 +3,37 @@
 command -v getcmdline > /dev/null || . /lib/dracut-lib.sh
 
 nm_generate_connections() {
-    rm -f /run/NetworkManager/system-connections/*
-    if [ -x /usr/libexec/nm-initrd-generator ]; then
-        # shellcheck disable=SC2046
-        /usr/libexec/nm-initrd-generator -- $(getcmdline)
-    elif [ -x /usr/lib/nm-initrd-generator ]; then
-        # shellcheck disable=SC2046
-        /usr/lib/nm-initrd-generator -- $(getcmdline)
+    if [ -e /usr/lib/systemd/system/NetworkManager-config-initrd.service ]; then
+        systemctl restart NetworkManager-config-initrd.service
     else
-        warn "nm-initrd-generator not found"
-    fi
+        rm -f /run/NetworkManager/system-connections/*
+        if [ -x /usr/libexec/nm-initrd-generator ]; then
+            # shellcheck disable=SC2046
+            /usr/libexec/nm-initrd-generator -- $(getcmdline)
+        elif [ -x /usr/lib/nm-initrd-generator ]; then
+            # shellcheck disable=SC2046
+            /usr/lib/nm-initrd-generator -- $(getcmdline)
+        else
+            warn "nm-initrd-generator not found"
+        fi
 
-    if getargbool 0 rd.neednet; then
-        for i in /usr/lib/NetworkManager/system-connections/* \
-            /run/NetworkManager/system-connections/* \
-            /etc/NetworkManager/system-connections/* \
-            /etc/sysconfig/network-scripts/ifcfg-*; do
-            [ -f "$i" ] || continue
-            echo '[ -f /tmp/nm.done ]' > "$hookdir"/initqueue/finished/nm.sh
-            mkdir -p /run/NetworkManager/initrd
-            : > /run/NetworkManager/initrd/neednet # activate NM services
-            break
-        done
+        if getargbool 0 rd.neednet; then
+            for i in /usr/lib/NetworkManager/system-connections/* \
+                /run/NetworkManager/system-connections/* \
+                /etc/NetworkManager/system-connections/* \
+                /etc/sysconfig/network-scripts/ifcfg-*; do
+                [ -f "$i" ] || continue
+                echo '[ -f /tmp/nm.done ]' > "$hookdir"/initqueue/finished/nm.sh
+                mkdir -p /run/NetworkManager/initrd
+                : > /run/NetworkManager/initrd/neednet # activate NM services
+                break
+            done
+        fi
     fi
 }
 
 nm_reload_connections() {
-    [ -n "$DRACUT_SYSTEMD" ] && systemctl is-active nm-initrd.service && nmcli connection reload
+    [ -n "$DRACUT_SYSTEMD" ] \
+        && systemctl -q is-active nm-initrd.service NetworkManager-initrd.service \
+        && nmcli connection reload
 }
