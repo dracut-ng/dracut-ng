9a180ba2481ea119fb8658684de0ebedea3ddeab
Jo Zzsi <jozzsicsataban@gmail.com>
2025-07-03 09:59:05 -0400

    test: remove unnecessary drivers from test dracut modules
    Fixes: https://github.com/dracut-ng/dracut-ng/issues/1324
    
    To increase test coverage for kernel-modules and instead
    of hard-coding required Linux kernel modules,
    use the kernel-modules dracut module instead.
    
    This change is enabled by 8730476 .

--- test/modules.d/70test/module-setup.sh
+++ test/modules.d/70test/module-setup.sh
@@ -6,17 +6,11 @@ check() {
 }
 
 depends() {
-    echo "base debug qemu watchdog"
+    echo "base debug qemu watchdog kernel-modules"
 }
 
+# testsuite assumes ext4 for convenience
 installkernel() {
     hostonly='' instmods \
-        ata_piix \
-        ext4 \
-        i6300esb \
-        ide-gd_mod \
-        piix \
-        sd_mod \
-        virtio_pci \
-        virtio_scsi
+        ext4
 }
