acc53ea19d08ff45a07c0e6f0eb446a410354ef7
Jo Zzsi <jozzsicsataban@gmail.com>
2025-07-13 01:35:51 -0400

    test(NETWORK): coverage for bringing up network interface
    Currently, all networking tests in the dracut CI require a client and a
    server VM, and all the server VMs are enabled to work with network-legacy only.
    
    Introduce a simpler networking test that tests the rd.neednet dracut
    boot time argument and ensures that the interface is up without needing
    a server VM.
    
    This simple networking test could be run in most networking configurations
    and pass in the Void test containers as well, which previously have not had
    any network tests enabled.

---
+++ test/TEST-50-NETWORK/assertion.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Get the output of 'ip addr show eth0'
+ip_output=$(ip addr show eth0)
+
+# Extract the line containing "inet"
+inet_line="${ip_output##*inet }"
+inet_line="${inet_line%% brd*}" # Remove everything after "brd" if it exists
+
+# Extract the IP address by removing the subnet mask
+ip_address="${inet_line%%/*}"
+
+# https://www.qemu.org/docs/master/system/devices/net.html#using-the-user-mode-network-stack
+# The qemu DHCP server assign addresses to the hosts starting from 10.0.2.15
+# We run the VM only with one network interface, so testing for 10.0.2.15 is safe
+
+if [ "$ip_address" != "10.0.2.15" ]; then
+    # fail the test
+    echo "ip addr show" >> /run/failed
+    ip addr show >> /run/failed
+fi
