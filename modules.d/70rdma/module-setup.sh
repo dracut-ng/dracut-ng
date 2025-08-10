c6bd4f059e2d1b0f4b6faf6847174242c0bf8345
Jo Zzsi <jozzsicsataban@gmail.com>
2025-08-10 10:43:14 -0400

    fix(rdma): install 90-rdma-umad.rules
    Always install 90-rdma-umad.rules (not just for RedHat).

--- modules.d/70rdma/module-setup.sh
+++ modules.d/70rdma/module-setup.sh
@@ -20,7 +20,7 @@ install() {
     inst /etc/rdma/modules/roce.conf
     inst /usr/libexec/mlx4-setup.sh
     inst_multiple lspci setpci awk sleep
-    inst_rules 60-rdma-persistent-naming.rules 70-persistent-ipoib.rules 75-rdma-description.rules 90-rdma-hw-modules.rules 90-rdma-ulp-modules.rules
+    inst_rules 60-rdma-persistent-naming.rules 70-persistent-ipoib.rules 75-rdma-description.rules 90-rdma-hw-modules.rules 90-rdma-ulp-modules.rules 90-rdma-umad.rules
     inst_multiple -o \
         "$systemdsystemunitdir"/rdma-hw.target \
         "$systemdsystemunitdir"/rdma-load-modules@.service
