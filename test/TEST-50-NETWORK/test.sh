5853d990ccee2ca33f053b63e7ad77fdfed9289b
Jo Zzsi <jozzsicsataban@gmail.com>
2025-08-11 22:47:52 -0400

    test(NETWORK): remove the hostonly_mode=sloppy workaround
    The test now passes regardless of the value of hostonly_mode.

--- test/TEST-50-NETWORK/test.sh
+++ test/TEST-50-NETWORK/test.sh
@@ -35,8 +35,7 @@ test_setup() {
     dd if=/dev/zero of="$TESTDIR"/root.img bs=200MiB count=1 status=none && sync "$TESTDIR"/root.img
     mkfs.ext4 -q -L dracut -d "$TESTDIR"/dracut.*/initramfs/ "$TESTDIR"/root.img && sync "$TESTDIR"/root.img
 
-    # TODO: remove the requirement for hostonly-mode=sloppy
-    test_dracut --hostonly-mode=sloppy --add "$USE_NETWORK"
+    test_dracut --add-drivers "virtio_net" --add "$USE_NETWORK"
 }
 
 # shellcheck disable=SC1090
