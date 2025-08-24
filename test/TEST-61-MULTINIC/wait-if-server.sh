1fda7e2983e4011c2d4f840427a6cf5ec3739d86
Jo Zzsi <jozzsicsataban@gmail.com>
2025-08-24 00:12:42 -0400

    test: remove unmaintained tests MULTINIC and BONDBRIDGEVLAN
    These tests are always skipped in all CI containers as they
    no longer pass. These tests require network-legacy and in some
    cases ifcfg and nobody stepped up to modernize them for years.
    
    Since the tests are in revision history, iof someone step up
    to maintain these tests, the project can easily bring them back.

--- test/TEST-61-MULTINIC/wait-if-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. /lib/net-lib.sh
-wait_for_if_link enx525400420001
--- test/TEST-62-BONDBRIDGEVLAN/wait-if-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-. /lib/net-lib.sh
-wait_for_if_link enx525400420001
-wait_for_if_link enx525400420002
-wait_for_if_link enx525400420003
-wait_for_if_link enx525400420004
