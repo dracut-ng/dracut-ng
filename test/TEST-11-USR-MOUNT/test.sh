f887fc2326eb618bbbd0260f59690557bba12979
Jo Zzsi <jozzsicsataban@gmail.com>
2025-09-06 22:40:47 -0400

    test(USR-MOUNT): improve readability by removing separate fstab file
    Let's test dracut's --mount command line option.

--- test/TEST-11-USR-MOUNT/test.sh
+++ test/TEST-11-USR-MOUNT/test.sh
@@ -49,7 +49,7 @@ test_setup() {
     # Create what will eventually be our root filesystem onto an overlay
     "$DRACUT" -N --keep --tmpdir "$TESTDIR" \
         --add-confdir test-root \
-        -i ./fstab /etc/fstab \
+        --mount "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_root /usr btrfs subvol=usr,ro" \
         -f "$TESTDIR"/initramfs.root
     mkdir -p "$TESTDIR"/overlay/source && mv "$TESTDIR"/dracut.*/initramfs/* "$TESTDIR"/overlay/source && rm -rf "$TESTDIR"/dracut.*
 
