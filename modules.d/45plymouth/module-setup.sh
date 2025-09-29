dracut-ng/dracut-ng

commit 1b374931126cc1cc4d731930c4b0ee520a4abbf6

fix(plymouth): do not depend on dpkg-architecture

modules.d/45plymouth/module-setup.sh
@@ -2,11 +2,8 @@
 
 pkglib_dir() {
     local _dirs="/usr/lib/plymouth /usr/libexec/plymouth/"
-    if find_binary dpkg-architecture &> /dev/null; then
-        local _arch
-        _arch=$(dpkg-architecture -qDEB_HOST_MULTIARCH 2> /dev/null)
-        [ -n "$_arch" ] && _dirs+=" /usr/lib/$_arch/plymouth"
-    fi
+    local _arch=${DRACUT_ARCH:-$(uname -m)}
+    [ -n "$_arch" ] && _dirs+=" /usr/lib/$_arch/plymouth"
     for _dir in $_dirs; do
         if [ -x "$dracutsysrootdir""$_dir"/plymouth-populate-initrd ]; then
             echo "$_dir"

commit ce4a2f37f189d378feda3d1ea909e556718e415b

fix(plymouth): check all library directories for Plymouth
On amd64 `dpkg-architecture -qDEB_HOST_MULTIARCH` returns
`x86_64-linux-gnu` but `uname -m` returns only `x86_64`.
`/usr/lib/x86_64` does not exist on Debian/Ubuntu but
`/usr/lib/x86_64-linux-gnu` exists.

Rely on `libdirs` to provide all possible library directories including
the Debian multiarch directory `/usr/lib/x86_64-linux-gnu`.

Fixes: 1b374931126c ("fix(plymouth): do not depend on dpkg-architecture")

modules.d/45plymouth/module-setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 pkglib_dir() {
-    local _dirs="/usr/lib/plymouth /usr/libexec/plymouth/"
-    local _arch=${DRACUT_ARCH:-$(uname -m)}
-    [ -n "$_arch" ] && _dirs+=" /usr/lib/$_arch/plymouth"
-    for _dir in $_dirs; do
-        if [ -x "${dracutsysrootdir-}$_dir"/plymouth-populate-initrd ]; then
-            echo "$_dir"
+    for _dir in /usr/libexec $libdirs; do
+        if [ -x "${dracutsysrootdir-}$_dir/plymouth/plymouth-populate-initrd" ]; then
+            echo "$_dir/plymouth"
             return
         fi
     done