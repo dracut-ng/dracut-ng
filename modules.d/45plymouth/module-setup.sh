dracut-ng/dracut-ng

commit 3a04a139700ffc7fe7a4897bb6067ebc46972874

fix: add bash dependency when bash scripts are used in the module
The following dracut modules are impacted:
 - systemd-networkd
 - plymouth
 - zipl

modules.d/45plymouth/module-setup.sh
@@ -24,7 +24,7 @@ check() {
 
 # called by dracut
 depends() {
-    echo drm
+    echo drm bash
 }
 
 # called by dracut

modules.d/01systemd-networkd/module-setup.sh
@@ -22,7 +22,7 @@ check() {
 depends() {
 
     # This module has external dependency on other module(s).
-    echo net-lib kernel-network-modules systemd-sysusers systemd
+    echo net-lib kernel-network-modules systemd-sysusers systemd bash
     # Return 0 to include the dependent module(s) in the initramfs.
     return 0
 

modules.d/91zipl/module-setup.sh
@@ -19,6 +19,11 @@ check() {
 }
 
 # called by dracut
+depends() {
+    echo bash
+}
+
+# called by dracut
 installkernel() {
     local _boot_zipl
 

commit 2d78106608ba49f6ecb044e8409115753e925e8f

fix(plymouth): drop depending on bash
The only Bash script in the Plymouth module is
`plymouth-populate-initrd.sh` which is called by `module-setup.sh` and
not included in the initrd. Therefore the Plymouth module does not
require Bash.

Fixes: 3a04a139700 ("fix: add bash dependency when bash scripts are used in the module")

modules.d/45plymouth/module-setup.sh
@@ -24,7 +24,7 @@ check() {
 
 # called by dracut
 depends() {
-    echo drm bash
+    echo drm
 }
 
 # called by dracut