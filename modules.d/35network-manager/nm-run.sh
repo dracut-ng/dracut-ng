dracut-ng/dracut-ng

commit 83dffc58f606ad7ad47a32716ce240831d7f018f

feat(network-manager): use upstream initrd services if available
Added in https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/commit/8640ea822f054c8817e0fd773ba7d1a8f42fa214

modules.d/35network-manager/nm-run.sh
@@ -14,10 +14,10 @@ if [ -z "$DRACUT_SYSTEMD" ]; then
             break
         done
     fi
-fi
 
-if [ -s /run/NetworkManager/initrd/hostname ]; then
-    cat /run/NetworkManager/initrd/hostname > /proc/sys/kernel/hostname
+    if [ -s /run/NetworkManager/initrd/hostname ]; then
+        cat /run/NetworkManager/initrd/hostname > /proc/sys/kernel/hostname
+    fi
 fi
 
 kf_get_string() {

modules.d/35network-manager/NetworkManager-config-initrd-dracut.conf
@@ -1,0 +1,27 @@
+[Unit]
+After=dracut-cmdline.service
+
+[Service]
+# Override ExecStart=, upstream parses /proc/cmdline, while dracut calls custom
+# `getcmdline` function to get cmdline options from /etc/cmdline and
+# /etc/cmdline.d/*.conf.
+# Also, search `nm-initrd-generator` in /usr/lib, for compatibility with distros
+# not using libexec.
+ExecStart=
+ExecStart=/bin/sh -c ' \
+    . /lib/dracut-lib.sh; \
+    if [ -x /usr/libexec/nm-initrd-generator ]; then \
+        /usr/libexec/nm-initrd-generator -- $(getcmdline); \
+    elif [ -x /usr/lib/nm-initrd-generator ]; then \
+        /usr/lib/nm-initrd-generator -- $(getcmdline); \
+    else \
+        warn "nm-initrd-generator not found"; \
+    fi \
+'
+# Complement ExecStartPost= to add an initqueue/finished hook.
+ExecStartPost=/bin/sh -c ' \
+    . /lib/dracut-lib.sh; \
+    if getargbool 0 rd.neednet && [ -e /run/NetworkManager/initrd/neednet ]; then \
+        echo "[ -f /tmp/nm.done ]" > "$hookdir"/initqueue/finished/nm.sh; \
+    fi \
+'

modules.d/35network-manager/NetworkManager-wait-online-initrd-dracut.conf
@@ -1,0 +1,2 @@
+[Unit]
+Before=dracut-initqueue.service

modules.d/35network-manager/module-setup.sh
@@ -48,16 +48,34 @@ install() {
         # "Wired connection #" DHCP connections for Ethernet interfaces
         inst_simple "$moddir"/initrd-no-auto-default.conf /usr/lib/NetworkManager/conf.d/
 
-        inst_simple "$moddir"/nm-initrd.service "$systemdsystemunitdir"/nm-initrd.service
-        inst_simple "$moddir"/nm-wait-online-initrd.service "$systemdsystemunitdir"/nm-wait-online-initrd.service
+        # Install systemd service units
+        if [[ -e "$systemdsystemunitdir"/NetworkManager-config-initrd.service ]]; then
+            # NetworkManager-1.54 provides its own initrd services
+            inst_multiple -o \
+                "$systemdsystemunitdir"/NetworkManager-config-initrd.service \
+                "$systemdsystemunitdir"/NetworkManager-initrd.service \
+                "$systemdsystemunitdir"/NetworkManager-wait-online-initrd.service
+
+            # dracut specific dropins to override upstream systemd services
+            inst_simple "$moddir/NetworkManager-config-initrd-dracut.conf" \
+                "$systemdsystemunitdir/NetworkManager-config-initrd.service.d/NetworkManager-config-initrd-dracut.conf"
+            inst_simple "$moddir/NetworkManager-wait-online-initrd-dracut.conf" \
+                "$systemdsystemunitdir/NetworkManager-wait-online-initrd.service.d/NetworkManager-wait-online-initrd-dracut.conf"
+
+            $SYSTEMCTL -q --root "$initdir" enable NetworkManager-initrd.service
+        else
+            #TODO: remove custom systemd services when NetworkManager-1.54 is the minimum supported version
+            inst_simple "$moddir"/nm-initrd.service "$systemdsystemunitdir"/nm-initrd.service
+            inst_simple "$moddir"/nm-wait-online-initrd.service "$systemdsystemunitdir"/nm-wait-online-initrd.service
+
+            $SYSTEMCTL -q --root "$initdir" enable nm-initrd.service
+        fi
 
         # Adding default link and (if exists) 98-default-mac-none.link
         inst_multiple -o \
             "${systemdnetwork}/99-default.link" \
             "${systemdnetwork}/98-default-mac-none.link"
         [[ $hostonly ]] && inst_multiple -H -o "${systemdnetworkconfdir}/*.link"
-
-        $SYSTEMCTL -q --root "$initdir" enable nm-initrd.service
     fi
 
     inst_hook initqueue/settled 99 "$moddir/nm-run.sh"

modules.d/35network-manager/nm-config.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-command -v nm_generate_connections > /dev/null || . /lib/nm-lib.sh
+command -v getargbool > /dev/null || . /lib/dracut-lib.sh
+
+[ -e /usr/lib/systemd/system/NetworkManager-initrd.service ] \
+    && nm_service_name="NetworkManager-initrd" \
+    || nm_service_name="nm-initrd"
 
 if [ -n "$netroot" ] || [ -e /tmp/net.ifaces ]; then
     echo rd.neednet >> /etc/cmdline.d/35-neednet.conf
@@ -22,8 +26,8 @@ if getargbool 0 rd.debug; then
         # See https://github.com/coreos/fedora-coreos-tracker/issues/943
         # shellcheck disable=SC2217
         if [ -w /dev/console ] && (echo < /dev/console) > /dev/null 2> /dev/null; then
-            mkdir -p /run/systemd/system/nm-initrd.service.d
-            cat << EOF > /run/systemd/system/nm-initrd.service.d/tty-output.conf
+            mkdir -p /run/systemd/system/"$nm_service_name".service.d
+            cat << EOF > /run/systemd/system/"$nm_service_name".service.d/tty-output.conf
 [Service]
 StandardOutput=tty
 EOF
@@ -32,4 +36,7 @@ EOF
     fi
 fi
 
-nm_generate_connections
+if [ "$nm_service_name" = "nm-initrd" ]; then
+    command -v nm_generate_connections > /dev/null || . /lib/nm-lib.sh
+    nm_generate_connections
+fi

modules.d/35network-manager/nm-lib.sh
@@ -3,31 +3,37 @@
 command -v getcmdline > /dev/null || . /lib/dracut-lib.sh
 
 nm_generate_connections() {
-    rm -f /run/NetworkManager/system-connections/*
-    if [ -x /usr/libexec/nm-initrd-generator ]; then
-        # shellcheck disable=SC2046
-        /usr/libexec/nm-initrd-generator -- $(getcmdline)
-    elif [ -x /usr/lib/nm-initrd-generator ]; then
-        # shellcheck disable=SC2046
-        /usr/lib/nm-initrd-generator -- $(getcmdline)
+    if [ -e /usr/lib/systemd/system/NetworkManager-config-initrd.service ]; then
+        systemctl restart NetworkManager-config-initrd.service
     else
-        warn "nm-initrd-generator not found"
-    fi
+        rm -f /run/NetworkManager/system-connections/*
+        if [ -x /usr/libexec/nm-initrd-generator ]; then
+            # shellcheck disable=SC2046
+            /usr/libexec/nm-initrd-generator -- $(getcmdline)
+        elif [ -x /usr/lib/nm-initrd-generator ]; then
+            # shellcheck disable=SC2046
+            /usr/lib/nm-initrd-generator -- $(getcmdline)
+        else
+            warn "nm-initrd-generator not found"
+        fi
 
-    if getargbool 0 rd.neednet; then
-        for i in /usr/lib/NetworkManager/system-connections/* \
-            /run/NetworkManager/system-connections/* \
-            /etc/NetworkManager/system-connections/* \
-            /etc/sysconfig/network-scripts/ifcfg-*; do
-            [ -f "$i" ] || continue
-            echo '[ -f /tmp/nm.done ]' > "$hookdir"/initqueue/finished/nm.sh
-            mkdir -p /run/NetworkManager/initrd
-            : > /run/NetworkManager/initrd/neednet # activate NM services
-            break
-        done
+        if getargbool 0 rd.neednet; then
+            for i in /usr/lib/NetworkManager/system-connections/* \
+                /run/NetworkManager/system-connections/* \
+                /etc/NetworkManager/system-connections/* \
+                /etc/sysconfig/network-scripts/ifcfg-*; do
+                [ -f "$i" ] || continue
+                echo '[ -f /tmp/nm.done ]' > "$hookdir"/initqueue/finished/nm.sh
+                mkdir -p /run/NetworkManager/initrd
+                : > /run/NetworkManager/initrd/neednet # activate NM services
+                break
+            done
+        fi
     fi
 }
 
 nm_reload_connections() {
-    [ -n "$DRACUT_SYSTEMD" ] && systemctl is-active nm-initrd.service && nmcli connection reload
+    [ -n "$DRACUT_SYSTEMD" ] \
+        && systemctl -q is-active nm-initrd.service NetworkManager-initrd.service \
+        && nmcli connection reload
 }

commit 3406f60e89232ddbcc136cd6301db91695c50c90

fix(network-manager): hostname not set with old systemd service units
Regression introduced in 83dffc58f606ad7ad47a32716ce240831d7f018f

Fixes #1743

modules.d/35network-manager/nm-run.sh
@@ -13,10 +13,11 @@ if [ -z "${DRACUT_SYSTEMD-}" ]; then
             break
         done
     fi
+fi
 
-    if [ -s /run/NetworkManager/initrd/hostname ]; then
-        cat /run/NetworkManager/initrd/hostname > /proc/sys/kernel/hostname
-    fi
+if [ -e /usr/lib/systemd/system/nm-initrd.service ] \
+    && [ -s /run/NetworkManager/initrd/hostname ]; then
+    cat /run/NetworkManager/initrd/hostname > /proc/sys/kernel/hostname
 fi
 
 kf_get_string() {