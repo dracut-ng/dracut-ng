4b403865be7aabbd95565be0e6eb813c01ad9800
Benjamin Drung <benjamin.drung@canonical.com>
2025-10-27 14:22:20 +0100

    test: name rootfs differently
    To help debugging issues, name the different rootfs' differently (i.e.
    include the name of the test and whether it is the server or client
    part).

--- test/TEST-72-NBD/client-init.sh
+++ test/TEST-72-NBD/client-init.sh
@@ -3,7 +3,7 @@
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 exec > /dev/console 2>&1
-echo "made it to the rootfs!"
+echo "made it to the NBD client rootfs!"
 
 while read -r dev fs fstype opts rest || [ -n "$dev" ]; do
     [ "$dev" = "rootfs" ] && continue
