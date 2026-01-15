94b21ab38fa50fc0e789f0bd2b76cead5da3fd10
Jo Zzsi <jozzsicsataban@gmail.com>
2026-01-15 14:09:59 -0500

    feat(qemu-net): auto-add qemu-net if net-lib and qemu included
    In the interest of improving defaults, if net-lib and qemu
    dracut modules are included than automatically include
    dracut-net dracut modules as well.
    
    I tested with the following dracut invocation
    
    `dracut -a "network qemu"`

--- modules.d/70qemu-net/module-setup.sh
+++ modules.d/70qemu-net/module-setup.sh
@@ -2,6 +2,11 @@
 
 # called by dracut
 check() {
+    if [[ $hostonly_mode != "strict" ]] && dracut_module_included "net-lib" \
+        && dracut_module_included "qemu"; then
+        return 0
+    fi
+
     is_qemu_virtualized && return 0
 
     if [[ $hostonly ]] || [[ $mount_needs ]]; then
