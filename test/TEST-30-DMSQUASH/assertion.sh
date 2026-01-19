cc669ad766f7271ddd388931bca178f1b25957d0
Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
2026-01-19 10:59:44 +0100

    test(DMSQUASH): add sync after writing the marker
    The change is done to ensure it is flushed to the persist
    partition before shutdown.

--- test/TEST-30-DMSQUASH/assertion.sh
+++ test/TEST-30-DMSQUASH/assertion.sh
@@ -4,4 +4,6 @@ if grep -qF ' rd.live.overlay=LABEL=persist ' /proc/cmdline; then
     # Writing to a file in the root filesystem lets test_run() verify that the autooverlay module successfully created
     # and formatted the overlay partition and that the dmsquash-live module used it when setting up the rootfs overlay.
     echo "dracut-autooverlay-success" > /overlay-marker
+    # Ensure the marker is flushed to disk before shutdown
+    sync /overlay-marker
 fi
