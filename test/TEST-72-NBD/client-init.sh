ff95609ea85ee5fe28bda4adb0f80ef1cfe8182c
Benjamin Drung <benjamin.drung@canonical.com>
2026-01-26 22:43:17 +0100

    test: use EXIT trap to poweroff client rootfs
    Use an trap on `EXIT` to poweroff the client rootfs. This will cover all
    kinds of failure and allows using `set -eu` in the init script.

--- test/TEST-72-NBD/client-init.sh
+++ test/TEST-72-NBD/client-init.sh
@@ -2,6 +2,21 @@
 : > /dev/watchdog
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
+# shellcheck disable=SC2317,SC2329  # called via EXIT trap
+_poweroff() {
+    echo "Powering down."
+
+    if [ -d /usr/lib/systemd/system ]; then
+        # graceful poweroff
+        systemctl start poweroff.target --job-mode=replace-irreversibly --no-block
+    else
+        # force immediate poweroff
+        poweroff -f
+    fi
+}
+
+trap _poweroff EXIT
 exec > /dev/console 2>&1
 echo "made it to the NBD client rootfs!"
 
@@ -16,13 +31,3 @@ done < /proc/mounts
 mount -n -o remount,ro /
 
 sync /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_marker
-
-echo "Powering down."
-
-if [ -d /usr/lib/systemd/system ]; then
-    # graceful poweroff
-    systemctl start poweroff.target --job-mode=replace-irreversibly --no-block
-else
-    # force immediate poweroff
-    poweroff -f
-fi
