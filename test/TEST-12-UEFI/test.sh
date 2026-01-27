c0047941cf2c0125962808956d9833ec7d280d0c
Benjamin Drung <benjamin.drung@canonical.com>
2026-01-27 23:18:54 +0100

    test(UEFI): let drive index start with 0
    Unify the tests and let the drive index for test 12 start at 0 as well.

--- test/TEST-12-UEFI/test.sh
+++ test/TEST-12-UEFI/test.sh
@@ -29,7 +29,7 @@ client_run() {
 
     declare -a disk_args=()
     # shellcheck disable=SC2034  # disk_index used in qemu_add_drive
-    declare -i disk_index=1
+    declare -i disk_index=0
     qemu_add_drive disk_index disk_args "$TESTDIR"/marker.img marker
     qemu_add_drive disk_index disk_args "$TESTDIR"/squashfs.img root
 
