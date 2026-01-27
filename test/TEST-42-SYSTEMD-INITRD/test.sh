57bacf782153c5b3abb6cd11febc2daec63112e6
Benjamin Drung <benjamin.drung@canonical.com>
2026-01-27 23:43:49 +0100

    test: use build_ext4_image where possible
    To speed up the test execution use `build_ext4_image` to build rootfs
    ext4 images in cases where QEMU was only used to build an unencrypted
    ext4 rootfs. This applies the improvement from commit 6e51e550abb8
    ("test(NFS): use build_ext4_image to build server rootfs") to all other
    test cases.

--- test/TEST-42-SYSTEMD-INITRD/test.sh
+++ test/TEST-42-SYSTEMD-INITRD/test.sh
@@ -35,33 +35,11 @@ test_run() {
 }
 
 test_setup() {
-    # Create what will eventually be our root filesystem onto an overlay
+    # Create client root filesystem
     call_dracut --tmpdir "$TESTDIR" \
         --add-confdir test-root \
         -f "$TESTDIR"/initramfs.root
-    mkdir -p "$TESTDIR"/overlay/source
-    mv "$TESTDIR"/dracut.*/initramfs/* "$TESTDIR"/overlay/source
-    rm -rf "$TESTDIR"/dracut.*
-
-    # create an initramfs that will create the target root filesystem.
-    # We do it this way so that we do not risk trashing the host mdraid
-    # devices, volume groups, encrypted partitions, etc.
-    call_dracut -i "$TESTDIR"/overlay / \
-        --add-confdir test-makeroot \
-        -i ./create-root.sh /lib/dracut/hooks/initqueue/01-create-root.sh \
-        -f "$TESTDIR"/initramfs.makeroot
-
-    declare -a disk_args=()
-    qemu_add_drive disk_args "$TESTDIR"/marker.img marker 1
-    qemu_add_drive disk_args "$TESTDIR"/root.img root 1
-
-    # Invoke KVM and/or QEMU to actually create the target filesystem.
-    "$testdir"/run-qemu \
-        "${disk_args[@]}" \
-        -append "root=/dev/fakeroot quiet" \
-        -initrd "$TESTDIR"/initramfs.makeroot
-    test_marker_check dracut-root-block-created
-    rm -- "$TESTDIR"/marker.img
+    build_ext4_image "$TESTDIR"/dracut.*/initramfs/ "$TESTDIR"/root.img dracut
 
     # initrd for required kernel modules
     # Improve boot time by generating two initrds. Do not re-compress kernel modules
