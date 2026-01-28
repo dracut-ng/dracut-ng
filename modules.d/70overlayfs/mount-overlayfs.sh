06dd20231f28e5a0b566ee69cbb69ad755ebc358
Benjamin Drung <benjamin.drung@canonical.com>
2026-01-28 22:47:19 +0100

    refactor(overlayfs): exit early in case overlayfs is not used
    Increase the code readability by exiting early in case overlayfs is not
    enabled and therefore should do nothing.

--- modules.d/70overlayfs/mount-overlayfs.sh
+++ modules.d/70overlayfs/mount-overlayfs.sh
@@ -5,14 +5,14 @@ command -v getarg > /dev/null || . /lib/dracut-lib.sh
 getargbool 0 rd.overlayfs -d rd.live.overlay.overlayfs && overlayfs="yes"
 getargbool 0 rd.overlay.readonly -d rd.live.overlayfs.readonly && readonly_overlay="--readonly" || readonly_overlay=""
 
-if [ -n "$overlayfs" ]; then
-    if [ -n "$readonly_overlay" ] && [ -h /run/overlayfs-r ]; then
-        ovlfs=lowerdir=/run/overlayfs-r:/run/rootfsbase
-    else
-        ovlfs=lowerdir=/run/rootfsbase
-    fi
+[ -n "$overlayfs" ] || return 0
 
-    if ! strstr "$(cat /proc/mounts)" LiveOS_rootfs; then
-        mount -t overlay LiveOS_rootfs -o "$ovlfs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
-    fi
+if [ -n "$readonly_overlay" ] && [ -h /run/overlayfs-r ]; then
+    ovlfs=lowerdir=/run/overlayfs-r:/run/rootfsbase
+else
+    ovlfs=lowerdir=/run/rootfsbase
+fi
+
+if ! strstr "$(cat /proc/mounts)" LiveOS_rootfs; then
+    mount -t overlay LiveOS_rootfs -o "$ovlfs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
 fi
