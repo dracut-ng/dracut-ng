1f823372aa00e131c88e860726462636beab71e0
Neal Gompa (ニール・ゴンパ) <ngompa13@gmail.com>
2026-02-04 05:03:18 -0500

    Revert "feat(base): support cmdline options in /run/initramfs/cmdline.d"
    This reverts commit bac8b3da6d8094d51b1cb4c6bb183e22cb001f12.

--- modules.d/80base/dracut-lib.sh
+++ modules.d/80base/dracut-lib.sh
@@ -10,7 +10,6 @@ fi
 
 if [ -z "${PREFIX-}" ]; then
     if ! [ -d /run/initramfs ]; then
-        mkdir -p -m 0755 /run/initramfs/cmdline.d
         mkdir -p -m 0755 /run/initramfs/log
         ln -sfn /run/initramfs/log /var/log
     fi
@@ -129,7 +128,6 @@ getcmdline() {
     local CMDLINE_ETC_D=''
     local CMDLINE_ETC=''
     local CMDLINE_PROC=''
-    local CMDLINE_RUN=''
     unset _line
 
     if [ -e /etc/cmdline ]; then
@@ -148,13 +146,7 @@ getcmdline() {
             CMDLINE_PROC="$CMDLINE_PROC $_line"
         done < /proc/cmdline
     fi
-    for _i in /run/initramfs/cmdline.d/*.conf; do
-        [ -e "$_i" ] || continue
-        while read -r _line || [ -n "$_line" ]; do
-            CMDLINE_RUN="$CMDLINE_RUN $_line"
-        done < "$_i"
-    done
-    CMDLINE="$CMDLINE_ETC_D $CMDLINE_ETC $CMDLINE_PROC $CMDLINE_RUN"
+    CMDLINE="$CMDLINE_ETC_D $CMDLINE_ETC $CMDLINE_PROC"
     printf "%s" "$CMDLINE"
 }
 
