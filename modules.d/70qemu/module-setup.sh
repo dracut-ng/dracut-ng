f4545c9e594833f239d2afe2a378c243459087cc
Jo Zzsi <jozzsicsataban@gmail.com>
2026-02-08 09:50:08 -0500

    chore(qemu): move is_qemu_virtualized to qemu dracut module
    Moving the is_qemu_virtualized function to qemu dracut module makes
    the code more modular and dracut.sh smaller.
    
    This change also enabled to remove the shellcheck exception.

--- modules.d/70qemu/module-setup.sh
+++ modules.d/70qemu/module-setup.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+is_qemu_virtualized() {
+    # 0 if a QEMU/KVM like VM virtualization environment was detected
+    # 1 if a QEMU/KVM like VM virtualization environment could not be detected (including if an error was encountered)
+
+    # do not consult /sys and do not detect virt environment in non-hostonly mode
+    ! [[ ${hostonly-} ]] && return 1
+
+    if type -P systemd-detect-virt > /dev/null 2>&1; then
+        if ! vm=$(systemd-detect-virt --vm 2> /dev/null); then
+            return 1
+        fi
+        [[ $vm == "qemu" ]] && return 0
+        [[ $vm == "kvm" ]] && return 0
+        [[ $vm == "bochs" ]] && return 0
+    fi
+
+    for i in /sys/class/dmi/id/*_vendor; do
+        [[ -f $i ]] || continue
+        read -r vendor < "$i"
+        [[ $vendor == "QEMU" ]] && return 0
+        [[ $vendor == "Red Hat" ]] && return 0
+        [[ $vendor == "Bochs" ]] && return 0
+    done
+    return 1
+}
+
 # called by dracut
 check() {
     is_qemu_virtualized && return 0
