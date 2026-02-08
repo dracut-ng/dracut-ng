dracut-ng/dracut-ng

commit 77c0d5774b02ee3b5972899766fa1fe74d645fd3

fix(release): use correct arguments for head(1) and tail(1) calls
This ensures that the NEWS fragments can be produced correctly.

tools/release.sh
@@ -34,8 +34,8 @@ make AUTHORS
 if ! type -p clog &> /dev/null; then
     cargo install clog-cli --version 0.9.3
 fi
-head -2 NEWS.md > NEWS_header.md
-tail +2 NEWS.md > NEWS_body.md
+head --lines=-2 NEWS.md > NEWS_header.md
+tail --lines=+2 NEWS.md > NEWS_body.md
 printf "dracut-ng-%s\n=============\n" "$NEW_VERSION" > NEWS_header_new.md
 
 # Append the list to the section in `NEWS.md`

commit 2bbf285ddec507ff07a58f72701ad69276cc7aaa

fix(release): use correct argument for head(1) to split header out
We actually want it to take the top two lines for identifying the header.

Fixes: 77c0d5774b ("fix(release): use correct arguments for head(1) and tail(1) calls")

tools/release.sh
@@ -34,7 +34,7 @@ make AUTHORS
 if ! type -p clog &> /dev/null; then
     cargo install clog-cli --version 0.9.3
 fi
-head --lines=-2 NEWS.md > NEWS_header.md
+head --lines=2 NEWS.md > NEWS_header.md
 tail --lines=+2 NEWS.md > NEWS_body.md
 printf "dracut-ng-%s\n=============\n" "$NEW_VERSION" > NEWS_header_new.md
 