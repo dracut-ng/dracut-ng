3636426c1258cf8cdd484c4f1aef6fca970fbd35
Benjamin Drung <benjamin.drung@canonical.com>
2026-02-11 14:33:51 +0100

    fix(kernel-modules-export): use return instead of exit in pre-pivot hook
    The `modules-export.sh` pre-pivot hook calls `exit 0`. This causes the
    sourcing script (`dracut-pre-pivot.sh` on systemd or `init.sh` from the
    base Dracut module) to exit. The subsequent hooks are not executed.
    
    Use `return` instead of `exit` in kernel-modules-export pre-pivot hook.
    
    Co-authored-by: Neal Gompa <neal@gompa.dev>
    Fixes: https://github.com/dracut-ng/dracut-ng/issues/2227

--- modules.d/70kernel-modules-export/modules-export.sh
+++ modules.d/70kernel-modules-export/modules-export.sh
@@ -3,7 +3,7 @@
 command -v getarg > /dev/null || . /lib/dracut-lib.sh
 
 if ! getargbool 0 rd.driver.export; then
-    exit 0
+    return 0
 fi
 
 driver_export=$(getarg rd.driver.export=)
@@ -12,19 +12,19 @@ KVERSION="$(uname -r)"
 
 if [ ! -d "/lib/modules/$KVERSION" ]; then
     warn "Something odd, no /lib/modules/$KVERSION in initramfs."
-    exit 0
+    return 0
 fi
 
 [ -d "$NEWROOT/lib/modules" ] || mkdir -p "$NEWROOT/lib/modules" \
     || {
         warn "No /lib/modules in target. cannot help."
-        exit 0
+        return 0
     }
 
 if [ -d "$NEWROOT/lib/modules/$KVERSION" ]; then
     if [ "${driver_export#*force}" = "$driver_export" ]; then
         warn "/lib/modules/$KVERSION exists. To export modules set rd.driver.export=force!"
-        exit 0
+        return 0
     else
         info "Due to rd.driver.export=force exporting over existing modules!"
     fi
@@ -33,11 +33,11 @@ fi
 mount -t tmpfs driver_export "$NEWROOT/lib/modules" \
     || {
         warn "Failed mount of tmpfs."
-        exit 0
+        return 0
     }
 
 cp -a "/lib/modules/$KVERSION" "$NEWROOT/lib/modules" \
     || {
         warn "Failed to export modules to target root."
-        exit 0
+        return 0
     }
