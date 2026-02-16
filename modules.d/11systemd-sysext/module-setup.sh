f9e00ff8940a3f65ffdef508ac95267e0e33ac93
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2026-02-16 16:12:21 +0100

    perf(systemd-sysext): do not attempt to install extension metadata
    Files under /usr/lib/extension-release.d/extension-release.* and
    /etc/extension-release.d/extension-release.* are self contained in sysexts and
    confext respectively. The important file to check if an extension can be
    installed is the os-release.

--- modules.d/11systemd-sysext/module-setup.sh
+++ modules.d/11systemd-sysext/module-setup.sh
@@ -41,7 +41,6 @@ install() {
     # regular files, but not with directory trees.
 
     inst_multiple -o \
-        "/usr/lib/extension-release.d/extension-release.*" \
         "$systemdsystemunitdir"/systemd-confext${_suffix}.service \
         "$systemdsystemunitdir/systemd-confext${_suffix}.service.d/*.conf" \
         "$systemdsystemunitdir"/systemd-sysext${_suffix}.service \
@@ -63,11 +62,9 @@ install() {
     # Install the hosts local user configurations if enabled.
     if [[ $hostonly ]]; then
         inst_multiple -H -o \
-            "/etc/extension-release.d/extension-release.*" \
             "$systemdsystemconfdir"/systemd-confext${_suffix}.service \
             "$systemdsystemconfdir/systemd-confext${_suffix}.service.d/*.conf" \
             "$systemdsystemconfdir"/systemd-sysext${_suffix}.service \
             "$systemdsystemconfdir/systemd-sysext${_suffix}.service.d/*.conf"
     fi
-
 }
