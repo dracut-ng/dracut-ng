24e0aa0d75478d15fd476353e77ac6b7671fce02
Martin Wilck <mwilck@suse.com>
2026-02-16 20:11:26 +0100

    feat(nvmf): add dracut.conf option nvmf_nbft_mode
    Add a dracut.conf.d option to control the generation of rd.nvmf.discover=
    entries if hostonly_cmdline = "yes".
    
    The option can take the values "static" (previous default behavior,
    generate cmdline entries for every NVMe subsystem in the device stack),
    "nbft" (don't generate any for tcp transport if an NBFT is present),
    and "match" (only generate rd.nvmf.discover= entries for subsystems that
    are not matched by current NBFT entries, as implemented by the previous
    commit).
    
    The default is "match", because this should fit user expectations best.
    
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- modules.d/74nvmf/module-setup.sh
+++ modules.d/74nvmf/module-setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+__nvmf_has_nbft() {
+    local f found=
+    for f in /sys/firmware/acpi/tables/NBFT*; do
+        [ -f "$f" ] || continue
+        found=1
+        break
+    done
+    [[ $found ]]
+}
+
 # called by dracut
 check() {
     local -A nvmf_trtypes
@@ -32,16 +42,6 @@ check() {
         fi
     }
 
-    has_nbft() {
-        local f found=
-        for f in /sys/firmware/acpi/tables/NBFT*; do
-            [ -f "$f" ] || continue
-            found=1
-            break
-        done
-        [[ $found ]]
-    }
-
     [[ $hostonly ]] || [[ $mount_needs ]] && {
         [ -f /etc/nvme/hostnqn ] || return 255
         [ -f /etc/nvme/hostid ] || return 255
@@ -53,7 +53,7 @@ check() {
         require_kernel_modules "${!nvmf_trtypes[@]}" || return 1
         if [ ! -f /sys/class/fc/fc_udev_device/nvme_discovery ] \
             && [ ! -f /etc/nvme/discovery.conf ] \
-            && [ ! -f /etc/nvme/config.json ] && ! has_nbft; then
+            && [ ! -f /etc/nvme/config.json ] && ! __nvmf_has_nbft; then
             echo "No discovery arguments present"
             return 255
         fi
@@ -125,12 +125,14 @@ cmdline() {
                 [[ $i =~ ^trsvcid= ]] && trsvcid="${i#trsvcid=}"
             done
             [[ -z $traddr && -z $host_traddr && -z $trsvcid ]] && continue
+            [[ $trtype == tcp && $nvmf_nbft_mode == nbft ]] \
+                && __nvmf_has_nbft && continue
             # _nbft_subsystems is set in cmdline() scope below.
             # It lists all subsystems found in the NBFT in the format
             # "traddr,trsvcid"
             # If the subsystem found is listed in the NBFT, don't add it
             # explicitly to the command line.
-            if [[ $trtype == tcp ]]; then
+            if [[ $trtype == tcp && $nvmf_nbft_mode == match ]]; then
                 for nbft_entry in "${_nbft_subsystems[@]}"; do
                     if [[ "$traddr,$trsvcid" == "$nbft_entry" ]]; then
                         continue 2
