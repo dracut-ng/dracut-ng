dracut-ng/dracut-ng

commit 24fb4c28538b89c423a2d4e7c00ee6e016137f90

feat(dracut): make hostonly the default
Hostonly is already the default in the last dracut release,
but the default was set as a configuration file.

This commit changes that and sets the default inside the dracut.sh
script (unless of course a distribution or user overrides the default).

The hostonly configuration file is kept intentionally, but it is now
not used by the configure option by default.

dracut.sh
@@ -1319,8 +1319,12 @@ check_kernel_compress_support() {
     return $?
 }
 
-[[ ${hostonly-} == yes ]] && hostonly="-h"
-[[ ${hostonly-} != "-h" ]] && unset hostonly
+# hostonly shell variable defaults to "-h" if not specified
+if [[ ${hostonly-} == "no" ]]; then
+    unset hostonly
+else
+    hostonly="-h"
+fi
 
 if [[ ${hostonly-} ]]; then
     for i in /sys /proc /run /dev; do

configure
@@ -39,9 +39,6 @@ case " $ID $ID_LIKE " in
     *" suse "*)
         configprofile=opensuse
         ;;
-    *)
-        configprofile=hostonly
-        ;;
 esac
 
 # Little helper function for reading args from the commandline.

dracut.conf.d/debian/01-debian.conf
@@ -1,2 +1,1 @@
 # Debian/Ubuntu specific Dracut configuration
-hostonly="yes"

commit 4b1aad2fbcad2bcc447bbb3f7caec59eca08fb7f

fix(dracut): enable hostonly_cmdline in hostonly mode again
A fresh Ubuntu 26.04 installation with a RAID 1 fails to boot due to not
assembling the RAID, because it neither sets `rd.auto` nor includes the
cmdline (with `rd.md.uuid` set).

The documentation for `hostonly_cmdline` says:

> If **hostonly="yes"** and this option is not configured, it's
> automatically set to "yes".

When `hostonly=yes` was made the default, `hostonly_cmdline` was not set
to `yes` any more in the default case.

Bug-Ubuntu: https://launchpad.net/bugs/2144948
Fixes: 24fb4c28538b ("feat(dracut): make hostonly the default")

dracut.sh
@@ -1138,7 +1138,7 @@ drivers_dir="${drivers_dir%"${drivers_dir##*[!/]}"}"
 [[ $hostonly_l ]] && hostonly=$hostonly_l
 [[ $hostonly_cmdline_l ]] && hostonly_cmdline=$hostonly_cmdline_l
 [[ $hostonly_mode_l ]] && hostonly_mode=$hostonly_mode_l
-[[ ${hostonly-} == "yes" ]] && ! [[ $hostonly_cmdline ]] && hostonly_cmdline="yes"
+[[ ${hostonly-} != "no" ]] && ! [[ $hostonly_cmdline ]] && hostonly_cmdline="yes"
 # shellcheck disable=SC2034
 [[ $i18n_install_all_l ]] && i18n_install_all=$i18n_install_all_l
 # shellcheck disable=SC2034