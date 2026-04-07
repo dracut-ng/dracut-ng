dracut-ng/dracut-ng

commit 04af3c097797d102934bc7ec666ed8eef00af9bb

fix(dracut): enable hostonly_cmdline in hostonly mode again
A fresh Ubuntu 26.04 installation with a RAID 1 fails to boot due to not
assembling the RAID, because it neither sets `rd.auto` nor includes the
cmdline (with `rd.md.uuid` set).

The documentation for `hostonly_cmdline` says:

> If **hostonly="yes"** and this option is not configured, it's
> automatically set to "yes".

When `hostonly=yes` was made the default, `hostonly_cmdline` was not set
to `yes` any more in the default case.

Bug-Ubuntu: https://launchpad.net/bugs/2144948
Fixes: 24fb4c28538b ("feat(dracut): make hostonly the default")

dracut.sh
@@ -1138,7 +1138,7 @@ drivers_dir="${drivers_dir%"${drivers_dir##*[!/]}"}"
 [[ $hostonly_l ]] && hostonly=$hostonly_l
 [[ $hostonly_cmdline_l ]] && hostonly_cmdline=$hostonly_cmdline_l
 [[ $hostonly_mode_l ]] && hostonly_mode=$hostonly_mode_l
-[[ ${hostonly-} == "yes" ]] && ! [[ $hostonly_cmdline ]] && hostonly_cmdline="yes"
+[[ ${hostonly-} != "no" ]] && ! [[ $hostonly_cmdline ]] && hostonly_cmdline="yes"
 # shellcheck disable=SC2034
 [[ $i18n_install_all_l ]] && i18n_install_all=$i18n_install_all_l
 # shellcheck disable=SC2034

commit e7470158fa2951f27ecae36e456d259502f88350

fix(dracut): determine hostonly_cmdline after hostonly setting
Commit 04af3c097797 enabled `hostonly_cmdline` in host-only mode again,
but determining that was done too early. In case `/dev` is not mounted,
the host-only mode is turned off. At that point `hostonly_cmdline` has
already be enabled.

So determine the default for `hostonly_cmdline` after the final check
for `hostonly`.

Bug-Debian: https://bugs.debian.org/1132794
Fixes: 04af3c097797 ("fix(dracut): enable hostonly_cmdline in hostonly mode again")

dracut.sh
@@ -1141,7 +1141,6 @@ drivers_dir="${drivers_dir%"${drivers_dir##*[!/]}"}"
 [[ $hostonly_l ]] && hostonly=$hostonly_l
 [[ $hostonly_cmdline_l ]] && hostonly_cmdline=$hostonly_cmdline_l
 [[ $hostonly_mode_l ]] && hostonly_mode=$hostonly_mode_l
-[[ ${hostonly-} != "no" ]] && ! [[ $hostonly_cmdline ]] && hostonly_cmdline="yes"
 # shellcheck disable=SC2034
 [[ $i18n_install_all_l ]] && i18n_install_all=$i18n_install_all_l
 # shellcheck disable=SC2034
@@ -1355,6 +1354,11 @@ if [[ ${hostonly-} ]]; then
     elif ! findmnt --target "/sys" &> /dev/null; then
         dwarning "Hostonly mode with no /sys mounted!"
     fi
+fi
+
+if [[ ${hostonly-} ]] && ! [[ $hostonly_cmdline ]]; then
+    # Enable hostonly-cmdline by default in hostonly mode
+    hostonly_cmdline="yes"
 fi
 
 case $hostonly_mode in