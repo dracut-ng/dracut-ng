87f4ffd2f153b3cf026804bf483e5c13dcb54752
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2026-04-22 11:38:11 +0200

    fix(systemd-sysext): add dependency to systemd-veritysetup
    The common use case is a bootloader or a UEFI boot stub (e.g. systemd-stub(7))
    that follows the BLS adding extensions found in the ESP to the /.extra directory
    in the initrd. In this case, it's used by default a strict
    "root=signed+absent:usr=signed+absent" image policy (see
    systemd.image-policy(7)). This means, Verity authentication and a PKCS#7
    signature of the Verity root hash.

--- modules.d/11systemd-sysext/module-setup.sh
+++ modules.d/11systemd-sysext/module-setup.sh
@@ -17,7 +17,7 @@ check() {
 depends() {
 
     # This module has external dependency on other module(s).
-    echo systemd
+    echo systemd systemd-veritysetup
     # Return 0 to include the dependent module(s) in the initramfs.
     return 0
 
