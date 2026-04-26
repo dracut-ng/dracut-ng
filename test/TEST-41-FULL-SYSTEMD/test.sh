25e3b22b84e1adef3f73982817ec1894cd8ab5a4
Jo Zzsi <jozzsicsataban@gmail.com>
2026-04-26 18:22:54 -0400

    feat(systemd-sysusers-service): systemd-sysusers.service module
    This new dracut module allows to run systemd-sysusers as part of the initrd boot process.
    
    Including this module is the preferred way for openSUSE.
    
    Fixes: https://github.com/dracut-ng/dracut-ng/issues/1360

--- test/TEST-41-FULL-SYSTEMD/test.sh
+++ test/TEST-41-FULL-SYSTEMD/test.sh
@@ -57,7 +57,7 @@ test_setup() {
     trap "$(shopt -p globstar)" RETURN
     shopt -q -s globstar
 
-    local dracut_modules="resume systemd-udevd systemd-journald systemd-tmpfiles systemd-cryptsetup systemd-emergency systemd-ac-power systemd-coredump systemd-creds systemd-integritysetup systemd-ldconfig systemd-pstore systemd-repart systemd-sysext systemd-veritysetup systemd-hostnamed systemd-timedated"
+    local dracut_modules="resume systemd-udevd systemd-journald systemd-tmpfiles systemd-cryptsetup systemd-emergency systemd-ac-power systemd-coredump systemd-creds systemd-integritysetup systemd-ldconfig systemd-pstore systemd-repart systemd-sysext systemd-sysusers-service systemd-veritysetup systemd-hostnamed systemd-timedated"
 
     if [ -f /usr/lib/systemd/systemd-networkd ]; then
         dracut_modules="$dracut_modules systemd-network-management"
