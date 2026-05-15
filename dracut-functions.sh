4886f210796af437f6f765fc1ba9c1b6d631ec2e
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2026-05-15 16:23:34 +0200

    fix(dracut-functions.sh): filter temporary RPM files
    These files have the suffix `;%08x`, i.e., semicolon followed by 8 hexadecimal
    characters.

--- dracut-functions.sh
+++ dracut-functions.sh
@@ -1342,6 +1342,8 @@ inst_libdir_file() {
         for _dir in $libdirs; do
             for _i in "$@"; do
                 for _f in "${dracutsysrootdir-}$_dir"/$_i; do
+                    # Filter temporary RPM files
+                    [[ $_f == *";"???????? ]] && continue
                     [[ ${_f#"${dracutsysrootdir-}"} =~ $_pattern ]] || continue
                     [[ -s $_f ]] && _files+=("${_f#"${dracutsysrootdir-}"}")
                 done
@@ -1351,6 +1353,8 @@ inst_libdir_file() {
         for _dir in $libdirs; do
             for _i in "$@"; do
                 for _f in "${dracutsysrootdir-}$_dir"/$_i; do
+                    # Filter temporary RPM files
+                    [[ $_f == *";"???????? ]] && continue
                     [[ -s $_f ]] && _files+=("${_f#"${dracutsysrootdir-}"}")
                 done
             done
