481f56d85b0231c526fed72474815805334561ac
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2026-05-15 09:12:19 +0200

    fix(dracut-functions.sh): do not attempt to install zero-size library files
    Fixes #2441

--- dracut-functions.sh
+++ dracut-functions.sh
@@ -1343,7 +1343,7 @@ inst_libdir_file() {
             for _i in "$@"; do
                 for _f in "${dracutsysrootdir-}$_dir"/$_i; do
                     [[ ${_f#"${dracutsysrootdir-}"} =~ $_pattern ]] || continue
-                    [[ -e $_f ]] && _files+=("${_f#"${dracutsysrootdir-}"}")
+                    [[ -s $_f ]] && _files+=("${_f#"${dracutsysrootdir-}"}")
                 done
             done
         done
@@ -1351,7 +1351,7 @@ inst_libdir_file() {
         for _dir in $libdirs; do
             for _i in "$@"; do
                 for _f in "${dracutsysrootdir-}$_dir"/$_i; do
-                    [[ -e $_f ]] && _files+=("${_f#"${dracutsysrootdir-}"}")
+                    [[ -s $_f ]] && _files+=("${_f#"${dracutsysrootdir-}"}")
                 done
             done
         done
